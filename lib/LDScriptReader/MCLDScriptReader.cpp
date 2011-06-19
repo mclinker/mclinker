ae4faa7f5654ce0caa0d0b0f206c22445d816ef8
Jush Lu <jush.msn@gmail.com>
2011-06-20 00:03:32 +0800

    Adding MCLDScriptReader stub

---
+++ lib/LDScriptReader/MCLDScriptReader.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ *   The BOLD Project, Copyright (C), 2011 -                                 *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Jush Lu <jush.msn@gmail.com>                                            *
+ ****************************************************************************/
+#include <MCLDScriptReader.h>
+
+using namespace mcld;
+
+//==========================
+// MCLDScriptReader
+MCLDScriptReader::MCLDScriptReader( const MCLDInfo& pLDInfo )
+: m_LDInfo(pLDInfo)
+{
+}
