e9052e14e5dfba835a6c0643be6a7b48bfad5185
luba <lubatang@gmail.com>
2011-07-13 15:05:32 +0800

    make the output format of regression test pretty.

--- regtests/test.sh
+++ regtests/test.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+function pad_space
+{
+	local PREFIX=$1
+        # adjust the length of header
+	NEW_SPACE=${#PREFIX}
+        NEW_SPACE=`expr 25 - ${NEW_SPACE}`
+        PAD="";
+        if [ "${NEW_SPACE}" -gt "0" ]; then # add space
+                for (( i=0; i<${NEW_SPACE}; i=i+1 )); do
+                        PAD="${PAD} ";
+                done
+        fi
+	echo -n "${PAD}";
+}
+
 DIRS=`find . -maxdepth 1 -type d `
 TOPLEVEL=`pwd`
-echo "=========================="
+echo "================================================="
 for DIR in ${DIRS}; do
 	if [ "${DIR}" != "." ]; then
 		cd ${DIR}
 		source ./test.sh
 		TEST=`basename ${DIR}`
-		echo -n "#${TEST}		";
+		echo -n "${TEST}";
 		setup &> /dev/null;
 		RESULT1=$?
 		testcase &> /dev/null;
@@ -17,6 +32,7 @@ for DIR in ${DIRS}; do
 		RESULT3=$?
 		test ${RESULT1} -eq 0 && test ${RESULT2} -eq 0 && test ${RESULT3} -eq 0
 		RESULT=$?
+		pad_space "${TEST}"
 		if [ ${RESULT} -eq 0 ]; then
 			echo "Passed!";
 		else
