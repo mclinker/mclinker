d032e2fff236158f58dcbd5f5432aed656b25c79
luba <lubatang@gmail.com>
2011-07-14 16:20:33 +0800

    add SearchDirs class.

---
+++ include/mcld/Support/SearchDirs.h
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Luba Tang <lubatang@mediatek.com>                                       *
+ ****************************************************************************/
+#ifndef SEARCHDIRS_H
+#define SEARCHDIRS_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class SearchDirs
+ *  \brief SearchDirs contains the list of paths that MCLinker will search for archive libraries and control scripts.
+ *
+ *  \see
+ *  \author Luba Tang <lubatang@mediatek.com>
+ */
+class SearchDirs
+{
+
+};
+
+} // namespace of mcld
+
+#endif
+
---
+++ lib/Support/SearchDirs.cpp
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Luba Tang <lubatang@mediatek.com>                                       *
+ ****************************************************************************/
+#include <mcld/Support/SearchDirs.h>
+
+using namespace mcld;
+
+//==========================
+// SearchDirs
+
