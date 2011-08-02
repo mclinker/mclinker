2b694ac4ee4f8dbe89869fae53f4a0bdbd10e424
Duo <pinronglu@gmail.com>
2011-08-02 10:20:24 +0800

    initialize MCAsmInfo for LLVM 3.0svn's demand

--- src/main.cpp
+++ src/main.cpp
@@ -200,6 +200,9 @@ int main( int argc, char* argv[] )
   llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
   // Initialize targets first, so that --version shows registered targets.
   InitializeAllTargets();
+#ifdef LLVM_VERSION > 2
+  InitializeAllMCAsmInfos();
+#endif
   InitializeAllAsmPrinters();
   InitializeAllAsmParsers();
   mcld::InitializeAllTargets();
