302c02d95568158ea42b990e6e187cf6a3e68b2f
luba <lubatang@gmail.com>
2011-08-09 19:07:54 +0800

    Sorry, Luba is stupid.

---
+++ lib/MC/MCGNUArchiveReader.cpp
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Luba Tang <lubatang@mediatek.com>                                       *
+ ****************************************************************************/
+#include <mcld/MC/MCGNUArchiveReader.h>
+
+using namespace mcld;
+
+//==========================
+// MCGNUArchiveReader
+MCGNUArchiveReader::MCGNUArchiveReader()
+{
+}
+
+MCGNUArchiveReader::~MCGNUArchiveReader()
+{
+}
+
