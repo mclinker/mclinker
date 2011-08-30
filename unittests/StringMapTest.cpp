4bbe75fe3202a731b303ef0dd38d8dd77b9a0308
Nowar Gu <nowar100@gmail.com>
2011-08-30 11:17:09 +0800

    Improve StringMap unittest. Replace tab by 2 space.

--- unittests/StringMapTest.cpp
+++ unittests/StringMapTest.cpp
@@ -19,41 +19,55 @@ using namespace mcldtest;
 // Constructor can do set-up work for all test here.
 StringMapTest::StringMapTest()
 {
-	// create testee. modify it if need
-	m_pTestee = new StringMap<int>();
+  // create testee. modify it if need
+  m_pTestee = new StringMap<int>();
 }
 
 // Destructor can do clean-up work that doesn't throw exceptions here.
 StringMapTest::~StringMapTest()
 {
-	delete m_pTestee;
+  delete m_pTestee;
 }
 
 // SetUp() will be called immediately before each test.
 void StringMapTest::SetUp()
 {
-	m_pTestee->GetOrCreateValue("a", 1);
-	m_pTestee->GetOrCreateValue("b", 2);
-	m_pTestee->GetOrCreateValue("c", 3);
+  m_pTestee->GetOrCreateValue("a", 1);
+  m_pTestee->GetOrCreateValue("b", 2);
+  m_pTestee->GetOrCreateValue("c", 5566);
+  m_pTestee->GetOrCreateValue("c", 3);
 }
 
 // TearDown() will be called immediately after each test.
 void StringMapTest::TearDown()
 {
-	m_pTestee->clear();
+  m_pTestee->clear();
 }
 
 //==========================================================================//
 // Testcases
 //
 TEST_F( StringMapTest, size ) {
-	ASSERT_EQ(3, m_pTestee->size());
+  ASSERT_EQ(3, m_pTestee->size());
+  ASSERT_FALSE(m_pTestee->empty());
 }
 
 TEST_F( StringMapTest, traverse ) {
-	StringMap<int>::iterator num, nEnd = m_pTestee->end();
-	for (num = m_pTestee->begin(); num!=nEnd; ++num) {
-		cerr << *num << endl;
-	}
+  StringMap<int>::iterator num, nEnd = m_pTestee->end();
+  size_t count = 0;
+  for (num = m_pTestee->begin(); num!=nEnd; ++num) {
+    cerr << *num << endl;
+    ++count;
+  }
+  ASSERT_EQ(3, count);
 }
 
+TEST_F(StringMapTest, clear) {
+  m_pTestee->clear();
+  ASSERT_TRUE(m_pTestee->empty());
+  ASSERT_EQ(0, m_pTestee->size());
+  m_pTestee->GetOrCreateValue("a", 123);
+  m_pTestee->GetOrCreateValue("a", -5566);
+  ASSERT_EQ(1, m_pTestee->size());
+  ASSERT_FALSE(m_pTestee->empty());
+}
