239064e6d8f23558ad5ecc1cf59e6088c14062e5
TDYa127 <a127a127@gmail.com>
2011-09-01 19:14:01 +0800

    Add StringUnorderedMap draft(not complete).

---
+++ lib/LD/StringUnorderedMap.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   TDYa127 <a127a127@gmail.com>                                            *
+ ****************************************************************************/
+#include <stdint.h>
+#include <cassert>
+#include <mcld/LD/StringUnorderedMap.h>
+
+using namespace mcld;
+
+//==========================
+// StringUnorderedMap
+
+
