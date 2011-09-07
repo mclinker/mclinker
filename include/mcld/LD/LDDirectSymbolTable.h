a8300f3914b371e62a350fb2b8aec82d1b5bafaa
TDYa127 <a127a127@gmail.com>
2011-09-07 21:43:24 +0800

    Add direct symbol table draft.

---
+++ include/mcld/LD/LDDirectSymbolTable.h
@@ -0,0 +1,43 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   TDYa127 <a127a127@gmail.com>                                            *
+ ****************************************************************************/
+#ifndef LDDIRECTSYMBOLTABLE_H
+#define LDDIRECTSYMBOLTABLE_H
+#include <llvm/ADT/StringRef.h>
+#include <mcld/LD/LDSymbolTalbeIF.h>
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+class LDSymbol;
+
+
+/** \class LDDirectSymbolTable
+ *  \brief For generally used symbol table.
+ *
+ *  \see
+ *  \author TDYa127 <a127a127@gmail.com>
+ */
+class LDDirectSymbolTable : public LDSymbolTalbeIF
+{
+public:
+  LDDirectSymbolTable(StringTable *);
+  virtual void insertSymbol(llvm::StringRef) {}
+  virtual const_iterator begin() const {}
+  virtual iterator begin() {}
+  virtual const_iterator end() const {}
+  virtual iterator end() {}
+  virtual bool merge(const LDSymbolTalbeIF*) {}
+}
+
+} // namespace of mcld
+
+#endif
+
