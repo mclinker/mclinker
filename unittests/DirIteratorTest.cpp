d7895e40690b87418f14cb84fa13b9756e120308
Jush Lu <Jush.Lu@mediatek.com>
2011-09-07 10:15:48 +0800

    Add FIXME to unittests for "DirIteratorTest"
    Some bugs modifies the global value "errno" to non-zero.
    This makes readir() failed when daily build system runs unittest
    Remove the FIXME after fixing those bugs
    
    This unittest fails only when running unittests by daily build system

--- unittests/DirIteratorTest.cpp
+++ unittests/DirIteratorTest.cpp
@@ -9,6 +9,8 @@
  ****************************************************************************/
 #include <mcld/Support/Directory.h>
 #include "DirIteratorTest.h"
+#include "errno.h"
+
 using namespace std;
 using namespace mcld;
 using namespace mcld::sys::fs;
@@ -18,14 +20,19 @@ using namespace mcldtest;
 // Constructor can do set-up work for all test here.
 DirIteratorTest::DirIteratorTest()
 {
-	// create testee. modify it if need
-	m_pDir = new mcld::sys::fs::Directory(".");
+  //FIXME:Some bugs modifies the global value "errno" to non-zero.
+  //      This makes readir() failed when daily build system runs unittest
+  //      Remove this after fixing those bugs
+  errno = 0;
+
+  // create testee. modify it if need
+  m_pDir = new mcld::sys::fs::Directory(".");
 }
 
 // Destructor can do clean-up work that doesn't throw exceptions here.
 DirIteratorTest::~DirIteratorTest()
 {
-	delete m_pDir;
+  delete m_pDir;
 }
 
 // SetUp() will be called immediately before each test.
@@ -44,20 +51,21 @@ void DirIteratorTest::TearDown()
 TEST_F( DirIteratorTest, open_dir ) {
 	ASSERT_TRUE( m_pDir->isGood() );
 
-	Directory::iterator entry = m_pDir->begin();
-	Directory::iterator enEnd = m_pDir->end();
-	static int counter = 0;
-	while( entry!=enEnd ) {
-		if (0 == entry.path()) {
-			cerr << "\tpath is null" << endl;
-		}
-		else {
-			cerr << "\t" <<  counter << ":" << entry.path()->native() << "<EOL>" << endl;
-                }
-                cerr << "\t\titerator increase" << endl;
-		counter++;
-		++entry;
-	}
+  Directory::iterator entry = m_pDir->begin();
+  Directory::iterator enEnd = m_pDir->end();
+  static int counter = 0;
+  while( entry!=enEnd ) {
+    if (0 == entry.path()) {
+      cerr << "\tpath is null" << endl;
+    }
+    else {
+      cerr << "\t" <<  counter << ":" << entry.path()->native() << "<EOL>" << endl;
+    }
+
+    cerr << "\t\titerator increase" << endl;
+    counter++;
+    ++entry;
+  }
 }
 
 
