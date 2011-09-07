99543babb6db39bcaa4259344ba9056e6bf7f1ae
TDYa127 <a127a127@gmail.com>
2011-09-07 21:21:19 +0800

    Add Output symbol table draft.

---
+++ include/mcld/LD/LDOutputSymbolTable.h
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   TDYa127 <a127a127@gmail.com>                                            *
+ ****************************************************************************/
+#ifndef LDOUTPUTSYMBOLTABLE_H
+#define LDOUTPUTSYMBOLTABLE_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+class LDSymbol;
+
+
+/** \class LDOutputSymbolTable
+ *  \brief Output symbol table, for MCLDOutput.
+ *
+ *  \see
+ *  \author TDYa127 <a127a127@gmail.com>
+ */
+class LDOutputSymbolTable : public LDIOSymbolTableIF
+{
+public:
+  LDOutputSymbolTable(StringTable *);
+  virtual void insertSymbol(llvm::StringRef) {}
+  virtual const_iterator begin() const {}
+  virtual iterator begin() {}
+  virtual const_iterator end() const {}
+  virtual iterator end() {}
+  virtual bool merge(const LDSymbolTalbeIF*) {}
+}
+
+} // namespace of mcld
+
+#endif
+
---
+++ lib/LD/LDOutputSymbolTable.cpp
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   TDYa127 <a127a127@gmail.com>                                            *
+ ****************************************************************************/
+#include <mcld/LD/LDOutputSymbolTable.h>
+
+using namespace mcld;
+
+//==========================
+// LDOutputSymbolTable
+
