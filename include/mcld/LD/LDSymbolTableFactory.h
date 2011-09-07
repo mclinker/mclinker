47eab80a93cddba2ae838d28ba13f24b6d6f59a2
TDYa127 <a127a127@gmail.com>
2011-09-07 22:15:48 +0800

    Add symbol table factory draft.

---
+++ include/mcld/LD/LDSymbolTableFactory.h
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   TDYa127 <a127a127@gmail.com>                                            *
+ ****************************************************************************/
+#ifndef LDSYMBOLTABLEFACTORY_H
+#define LDSYMBOLTABLEFACTORY_H
+#include <mcld/MC/MCLDInput.h>
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class LDSymbolTableFactory
+ *  \brief LDSymbolTableFactory constructs the SymbolTables.
+ *
+ *  \see
+ *  \author TDYa127 <a127a127@gmail.com>
+ */
+template<typename bool global>
+class LDSymbolTableFactory
+{
+public:
+  LDSymbolTableFactory();
+  LDIOSymbolTableIF *Create(StringTable *, const Input&);
+  LDIOSymbolTableIF *Create(StringTable *, const Output&);
+  LDSymbolTableIF *Create(StringTable *);
+};
+
+} // namespace of mcld
+
+#endif
+
