1128fcfc51ef86d0ad8a9b02681fd773fbe59f9b
luba <lubatang@gmail.com>
2011-09-28 11:31:21 +0800

    Add mcld::Resolver to bind symbols.
    mcld::Resolver is the object who processes symbol resolution.

---
+++ include/mcld/LD/Resolver.h
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Luba Tang <lubatang@mediatek.com>                                       *
+ ****************************************************************************/
+#ifndef RESOLVER_H
+#define RESOLVER_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class Resolver
+ *  \brief Resolver binds a symbol reference from one file to a symbol
+ *   definition of another file.
+ *
+ */
+class Resolver
+{
+public:
+  /// shouldOverwrite - Can resolver overwrite the symbol pOld by the
+  /// symbol pNew?
+  //  @param pOld the symbol which may be overwrited.
+  //  @param pNew the symbol which is used to replace pOld
+  bool shouldOverwrite(const LDSymbol& pOld, const LDSymbol& pNew);
+};
+
+} // namespace of mcld
+
+#endif
+
---
+++ lib/LD/Resolver.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Luba Tang <lubatang@mediatek.com>                                       *
+ ****************************************************************************/
+#include <Resolver.h>
+
+using namespace mcld;
+
+//==========================
+// Resolver
+bool Resolver::shouldOverwrite(const LDSymbol& pOld, const LDSymbol& pNew)
+{
+}
+
