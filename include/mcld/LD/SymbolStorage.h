ce06c6bfc0d300ee120b9ebd9df05eb5b7ed67cd
luba <lubatang@gmail.com>
2011-10-03 11:52:14 +0800

    Add SymbolStorage under ${MCLinker}/LD
    This is an early prototype of SymbolStrage. Some trivial bugs may occur.

---
+++ include/mcld/LD/SymbolStorage.h
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   TDYa127 <a127a127@gmail.com>                                            *
+ ****************************************************************************/
+#ifndef MCLD_SYMBOL_STORAGE_H
+#define MCLD_SYMBOL_STORAGE_H
+#include <mcld/LD/StringTableIF.h>
+#include <mcld/LD/SymbolTableEntry.h>
+#include <mcld/LD/StringUnorderedMap.h>
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class SymbolStorage
+ *  \brief Store symbol and search symbol by name. Can help symbol resolution.
+ *
+ *  \see
+ *  \author TDYa127 <a127a127@gmail.com>
+ */
+class SymbolStorage
+{
+  /* draft. */
+  friend class SymbolTableFactory;
+
+  SymbolStorage(StringTableIF *pStrTab)
+  : m_Strtab(pStrTab), m_Allocator(256)
+  {}
+
+public:
+  typedef vector<LDSymbol *> SymbolList;
+
+  void insertSymbol(llvm::StringRef);
+  void merge(const SymbolStorage &);
+
+private:
+  typedef SymbolTableEntry<ShouldOverwrite> SymbolTableEntryType;
+  StringTableIF *m_Strtab;
+  SymbolList m_SymList;
+  GCFactory<SymbolTableEntryType> m_Allocator;
+
+  template<typename DataType>
+  class GCFactory256 : public GCFactory<DataType, 256>
+  {};
+
+private:
+  StringUnorderedMap<SymbolTableEntryType *,
+                     const char *,
+                     StringUnorderedMapDefaultHash,
+                     GCFactory256> m_SymbolSearch;
+private:
+  m_SymbolList *getSymbolList() {
+    return &m_SymbolList;
+  }
+};
+
+} // namespace of mcld
+
+#endif
+
---
+++ lib/LD/SymbolStorage.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   TDYa127 <a127a127@gmail.com>                                            *
+ ****************************************************************************/
+#include <mcld/LD/SymbolStorage.h>
+#include <mcld/LD/StringTableIF.h>
+#include <cassert>
+
+using namespace mcld;
+using namespace std;
+
+//==========================
+// SymbolStorage
+void SymbolStorage::insertSymbol(llvm::StringRef pSymName)
+{
+  /* TODO: insert. */
+}
+
+void SymbolStorage::merge(const SymbolStorage &pSymTab)
+{
+  if(this==&pSymTab) return;
+  assert(0 && "Not implemented.");
+  /* TODO: Efficiently merge. NOTE: The string table may be different. */
+}
