60858969b65dbeaac3b6eae2a85522eda133ba0b
Nowar Gu <nowar100@gmail.com>
2011-10-04 14:00:04 +0800

    Add unittest for StringTable.

---
+++ unittests/StringTableTest.h
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ *   Test Suite of The MCLinker Project,                                     *
+ *                                                                           *
+ *   Copyright (C), 2011 -                                                   *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Nowar Gu <nowar100@gmail.com>                                           *
+ ****************************************************************************/
+#ifndef STRINGTABLE_TEST_H
+#define STRINGTABLE_TEST_H
+
+#include <gtest.h>
+
+namespace mcld
+{
+class StringTable;
+
+} // namespace for mcld
+
+namespace mcldtest
+{
+
+/** \class StringTableTest
+ *  \brief 
+ *
+ *  \see StringTable 
+ */
+class StringTableTest : public ::testing::Test
+{
+public:
+  // Constructor can do set-up work for all test here.
+  StringTableTest();
+
+  // Destructor can do clean-up work that doesn't throw exceptions here.
+  virtual ~StringTableTest();
+
+  // SetUp() will be called immediately before each test.
+  virtual void SetUp();
+
+  // TearDown() will be called immediately after each test.
+  virtual void TearDown();
+
+protected:
+  mcld::StringTable* m_pTestee;
+};
+
+} // namespace of mcldtest
+
+#endif
+
