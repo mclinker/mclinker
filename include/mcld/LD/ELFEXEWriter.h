b634a59912a1d40ccf99bed32869dc715f0c997e
Jush Lu <Jush.Lu@mediatek.com>
2011-10-26 12:47:26 +0800

    Add ELFEXEWriter header and footer

---
+++ include/mcld/LD/ELFEXEWriter.h
@@ -0,0 +1,30 @@
+//===- ELFEXEWriter.h -----------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MCLD_LD_ELFEXEWRITER
+#define MCLD_LD_ELFEXEWRITER
+
+#include "mcld/LD/LDELFWriter.h"
+
+class MemoryArea;
+
+namespace mcld {
+class ELFEXEWriter : public LDELFWriter {
+public:
+  ELFEXEWriter(MemoryArea *Area, bool _IsLittleEndian)
+  : LDELFWriter(Area, _IsLittleEndian) {}
+
+  void writeFile();
+
+private:
+  void writeHeader();
+};
+} // end namespace mcld
+#endif
+
---
+++ lib/LD/ELFEXEWriter.cpp
@@ -0,0 +1,30 @@
+//===- ELFEXEWriter.cpp ---------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mcld/LD/ELFEXEWriter.h"
+
+using namespace mcld;
+
+void ELFEXEWriter::writeHeader(){
+  RegionWriter Writer(
+    m_pArea->request(0, sizeof(llvm::ELF::Elf32_Ehdr)), false);
+
+  Writer.Write8(0x7f); // e_ident[EI_MAG0]
+  Writer.Write8('E');  // e_ident[EI_MAG1]
+  Writer.Write8('L');  // e_ident[EI_MAG2]
+  Writer.Write8('F');  // e_ident[EI_MAG3]
+}
+
+
+void ELFEXEWriter::writeFile() {
+
+  writeHeader();
+
+}
+
