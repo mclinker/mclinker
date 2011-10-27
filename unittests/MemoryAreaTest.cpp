f07dc273f036e1fd1f7616275ff52d065a02378b
Jush Lu <Jush.Lu@mediatek.com>
2011-10-27 13:40:34 +0800

    Refine MemoryAreaTest
    Remove redundant tests, and change file names of sample files

--- unittests/MemoryAreaTest.cpp
+++ unittests/MemoryAreaTest.cpp
@@ -16,6 +16,7 @@
 
 #include "MemoryAreaTest.h"
 #include <fcntl.h>
+#include <cstdio>
 
 using namespace mcld;
 using namespace mcld::sys::fs;
@@ -45,109 +46,46 @@ void MemoryAreaTest::TearDown()
 //==========================================================================//
 // Testcases
 //
-TEST_F( MemoryAreaTest, should_fail_to_write_file ) {
-  unsigned char range[16];
-  MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1);
-
-  sys::fs::Path Path("/lib/libc.so.6");
-  m_pTestee = AreaFactory->produce(Path.c_str() ,O_WRONLY);
-  ASSERT_FALSE(m_pTestee->isMapped());
-}
-
-TEST_F( MemoryAreaTest, read_file ) {
-  unsigned char range[16];
-  MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1);
-
-  sys::fs::Path Path("/lib/libc.so.6");
-  m_pTestee = AreaFactory->produce(Path.c_str() ,O_RDONLY);
-
-  if (!m_pTestee->isMapped())
-    FAIL() << "file is not open.";
-  if (!m_pTestee->isGood())
-    FAIL() << "file is failed to open";
-
-  MemoryRegion* region = m_pTestee->request(0, sizeof(range));
-  if (!region)
-    FAIL() << "region is NULL";
-
-  const sys::fs::detail::Address magic = region->getBuffer();
-
-  if (magic){
-    if (magic[0] == 0x7F && magic[1] == 'E' &&
-        magic[2] == 'L' && magic[3] == 'F')
-      SUCCEED();
-    else
-      FAIL() << "magic number is not correctly read";
-  }
-  else
-    FAIL() << "magic is NULL";
-
-  delete AreaFactory;
-}
-
-TEST_F( MemoryAreaTest, read_a_ascii_file )
+TEST_F( MemoryAreaTest, read_by_malloc )
 {
 	Path path(TOPDIR);
-	path.append("unittests/test.txt");
+	path.append("unittests/MemoryArea-read.txt");
 	MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1);
 	MemoryArea* area = AreaFactory->produce(path, O_RDONLY);
 	MemoryRegion* region = area->request(3, 2);
-	ASSERT_EQ('s', region->getBuffer()[0]);
-	ASSERT_EQ(' ', region->getBuffer()[1]);
+	ASSERT_EQ('L', region->getBuffer()[0]);
+	ASSERT_EQ('O', region->getBuffer()[1]);
 	delete AreaFactory;
 }
 
-TEST_F( MemoryAreaTest, write_a_ascii_file )
+TEST_F( MemoryAreaTest, write_by_malloc )
 {
 	Path path(TOPDIR);
-	path.append("unittests/test2.txt");
-	MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1);
-	MemoryArea* area = AreaFactory->produce(path, O_RDWR);
-        ASSERT_TRUE(area->isMapped());
-	MemoryRegion* region = area->request(3, 4);
-	region->getBuffer()[0] = 'L';
-	region->getBuffer()[1] = 'u';
-	region->getBuffer()[2] = 'b';
-	region->getBuffer()[3] = 'a';
-	region->sync();
-	area->unmap();
-        area->map(path, O_RDONLY);
-        ASSERT_TRUE(area->isMapped());
-        ASSERT_TRUE(area->isGood());
-        region = area->request(5, 2);
-        ASSERT_EQ('b', region->getBuffer()[0]);
-        ASSERT_EQ('a', region->getBuffer()[1]);
-	delete AreaFactory;
-}
-
-TEST_F( MemoryAreaTest, write_a_ascii_file_2 )
-{
-	Path path(TOPDIR);
-	path.append("unittests/test2.txt");
+	path.append("unittests/MemoryArea-write.txt");
 	MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1);
 	MemoryArea* area = AreaFactory->produce(path, O_RDWR);
         ASSERT_TRUE(area->isMapped());
         ASSERT_TRUE(area->isGood());
 	MemoryRegion* region = area->request(3, 4);
 	region->getBuffer()[0] = 'L';
-	region->getBuffer()[1] = 'u';
-	region->getBuffer()[2] = 'b';
-	region->getBuffer()[3] = 'a';
+	region->getBuffer()[1] = 'i';
+	region->getBuffer()[2] = 'n';
+	region->getBuffer()[3] = 'k';
 	region->sync();
 	area->unmap();
         area->map(path, O_RDONLY);
         ASSERT_TRUE(area->isMapped());
         ASSERT_TRUE(area->isGood());
         region = area->request(5, 2);
-        ASSERT_EQ('b', region->getBuffer()[0]);
-        ASSERT_EQ('a', region->getBuffer()[1]);
+        ASSERT_EQ('n', region->getBuffer()[0]);
+        ASSERT_EQ('k', region->getBuffer()[1]);
 	delete AreaFactory;
 }
 
 TEST_F( MemoryAreaTest, read_one_page )
 {
         Path path(TOPDIR) ;
-	path.append("unittests/test3.txt") ;
+	path.append("unittests/MemoryArea-read.txt") ;
 	MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1) ;
 	MemoryArea* area = AreaFactory->produce(path, O_RDWR) ;
 	ASSERT_TRUE(area->isMapped()) ;
@@ -161,7 +99,7 @@ TEST_F( MemoryAreaTest, read_one_page )
 TEST_F( MemoryAreaTest, write_one_page )
 {
         Path path(TOPDIR) ;
-	path.append("unittests/test3.txt") ;
+	path.append("unittests/MemoryArea-write.txt") ;
         MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1) ;
 	MemoryArea* area = AreaFactory->produce(path, O_RDWR) ;
 	ASSERT_TRUE(area->isMapped()) ;
@@ -184,28 +122,27 @@ TEST_F( MemoryAreaTest, write_one_page )
 TEST_F( MemoryAreaTest, write_sync )
 {
         Path path(TOPDIR) ;
-	path.append("unittests/test3.txt") ;
-	MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1) ; 
+	path.append("unittests/MemoryArea-write.txt") ;
+	MemoryAreaFactory *AreaFactory = new MemoryAreaFactory(1) ;
 	MemoryArea* area = AreaFactory->produce(path, O_RDWR) ;
 	ASSERT_TRUE(area->isMapped()) ;
 	ASSERT_TRUE(area->isGood()) ;
 	MemoryRegion* region1 = area->request(0, 4096) ;
 	MemoryRegion* region2 = area->request(512, 1024) ;
-	region1->getBuffer()[1000] = 'X' ;
-	region1->getBuffer()[1001] = 'D' ;
-	region2->getBuffer()[488] = ':' ;
-	region2->getBuffer()[489] = ')' ;
+	region1->getBuffer()[1000] = 'L' ;
+	region1->getBuffer()[1001] = 'L' ;
+	region2->getBuffer()[488] = 'V' ;
+	region2->getBuffer()[489] = 'M' ;
 	region1->sync() ;
 	area->unmap();
 	area->map(path, O_RDWR) ;
 	region1 = area->request(0, 1024) ;
-	EXPECT_EQ(':', region1->getBuffer()[1000]) ;
-	EXPECT_EQ(')', region1->getBuffer()[1001]) ;
+	EXPECT_EQ('V', region1->getBuffer()[1000]) ;
+	EXPECT_EQ('M', region1->getBuffer()[1001]) ;
 	region1->getBuffer()[1000] = '@' ;
 	region1->getBuffer()[1001] = '@' ;
 	region1->sync();
 	delete AreaFactory ;
 }
 
-   
 
