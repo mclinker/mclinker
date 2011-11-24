c3b40a09c7ad181d9dd9af19c50a0c70a8f743da
luba <lubatang@gmail.com>
2011-11-24 11:40:05 +0800

    Add the scaffolding of PZString and PZStringFactory.
    PZString is a Pascal string with zero terminal.
    Because PZString is a kind of class whose size is various, it's only
    can be put in dynamic storage. Therefore, it can not use normal allocator.

---
+++ include/mcld/ADT/PZStringFactory.h
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Diana Chen <diana.chen@mediatek.com>                                    *
+ *   Luba Tang <lubatang@mediatek.com>                                       *
+ ****************************************************************************/
+#ifndef PASCAL_ZERO_END_STRING_FACTORY_H
+#define PASCAL_ZERO_END_STRING_FACTORY_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class PZStringFactory
+ *  \brief PZStringFactory is the factory for PZString.
+ */
+class PZStringFactory
+{
+public:
+  PZStringFactory();
+  ~PZStringFactory();
+
+  
+};
+
+} // namespace of mcld
+
+#endif
+
---
+++ lib/ADT/PZString.cpp
@@ -0,0 +1,62 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Diana Chen <diana.chen@mediatek.com>                                    *
+ *   Luba Tang <lubatang@mediatek.com>                                       *
+ ****************************************************************************/
+#include <mcld/ADT/PZString.h>
+
+using namespace mcld;
+
+//==========================
+// PZString
+PZString::PZString()
+{
+}
+
+PZString::~PZString()
+{
+}
+
+PZString::iterator PZString::rbegin()
+{
+}
+
+PZString::iterator PZString::rend()
+{
+}
+
+PZString::const_iterator PZString::rbegin() const
+{
+}
+
+PZString::const_iterator PZString::rend() const
+{
+}
+
+PZString& PZString::assign(const char* pStr, PZString::Alloc& pFactory)
+{
+}
+
+PZString& PZString::assign(const char* pStr, size_type pLength, PZString::Alloc& pFactory)
+{
+}
+
+PZString& PZString::assign(const char* pStart, const char* pLast, PZString::Alloc& pFactory)
+{
+}
+
+PZString& PZString::assign(const std::string& pStr, PZString::Alloc& pFactory)
+{
+}
+
+PZString& PZString::assign(const llvm::StringRef& pStr, PZString::Alloc& pFactory)
+{
+}
+
+PZString& PZString::assign(const PZString& pCopy, PZString::Alloc& pFactory)
+{
+}
+
