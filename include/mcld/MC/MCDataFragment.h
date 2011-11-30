12825372ce875ccc72d30bfd4e2f17f3c49d5c31
Diana Chen <diana.chen@mediatek.com>
2011-11-30 18:02:24 +0800

    Add MCDataFragment and MCInstFragment

---
+++ include/mcld/MC/MCDataFragment.h
@@ -0,0 +1,82 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Diana Chen <diana.chen@mediatek.com>                                    *
+ ****************************************************************************/
+#ifndef MCDATAFRAGMENT_H
+#define MCDATAFRAGMENT_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+#include <llvm/MC/MCAssembler.h>
+#include <llvm/MC/MCInst.h>
+#include <llvm/ADT/SmallString.h>
+#include <mcld/LD/LDRelocation.h>
+
+namespace mcld
+{
+
+/** \class MCDataFragment
+ *  \brief MCDataFragment for mcld
+ *
+ *  \see
+ *  \author Diana Chen <diana.chen@mediatek.com>
+ */
+class MCDataFragment : public  llvm::MCFragment
+{
+private:
+
+  ///  m_pFragment - llvm MCInstFragment for this MCInstFragment
+  llvm::MCDataFragment* m_pFragment;
+ 
+  /// m_Relocation - The list of relocations in this fragment
+  std::vector<LDRelocation> m_Relocations;
+
+public:
+  typedef std::vector<LDRelocation>::const_iterator const_relocation_iterator;
+  typedef std::vector<LDRelocation>::iterator relocation_iterator;
+
+public:
+  MCDataFragment(llvm::MCDataFragment& pFragment)
+    : m_pFragment(&pFragment) {
+    setParent( pFragment.getParent() );
+    setAtom( pFragment.getAtom() );
+    setLayoutOrder( pFragment.getLayoutOrder());
+  }
+  ~MCDataFragment(){}
+   
+  // ------ observers ------//
+  llvm::SmallString<32> &getContents() { return m_pFragment->getContents();  }
+  const llvm::SmallString<32> &getContents() const { return m_pFragment->getContents();  }
+
+  // relocation access
+  void addRelocation(LDRelocation pReloc){  m_Relocations.push_back(pReloc); }
+  
+  std::vector<LDRelocation> &getRelocations() { return m_Relocations; }
+  const std::vector<LDRelocation> &getRelcoations() const { return m_Relocations; }
+
+  relocation_iterator relocation_begin() { return m_Relocations.begin(); }
+  const_relocation_iterator relocation_begin() const { return m_Relocations.begin(); }
+
+  relocation_iterator relocation_end() {return m_Relocations.end();}
+  const_relocation_iterator relocation_end() const {return m_Relocations.end();}
+
+  size_t relocations_size() const { return m_Relocations.size(); }
+
+  // fragment identification
+  static bool classof(const MCFragment *pF) {
+    return pF->getKind() == llvm::MCFragment::FT_Data;
+  }
+  static bool classof(const MCDataFragment *) { return true; }
+  
+  // overwrite parent method
+  FragmentType getKind() const { return m_pFragment->getKind(); } 
+  
+};
+
+} // namespace of mcld
+
+#endif
+
---
+++ include/mcld/MC/MCInstFragment.h
@@ -0,0 +1,91 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Diana Chen <diana.chen@mediatek.com>                                    *
+ ****************************************************************************/
+#ifndef MCINSTFRAGMENT_H
+#define MCINSTFRAGMENT_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+#include <llvm/MC/MCAssembler.h>
+#include <llvm/MC/MCInst.h>
+#include <llvm/ADT/SmallString.h>
+#include <mcld/LD/LDRelocation.h>
+
+
+namespace mcld
+{
+
+/** \class MCInstFragment
+ *  \brief MCInstFragment for mcld
+ *
+ *  \see
+ *  \author Diana Chen <diana.chen@mediatek.com>
+ */
+class MCInstFragment : public  llvm::MCFragment
+{
+private:
+
+  ///  m_pFragment - llvm MCInstFragment for this MCInstFragment
+  llvm::MCInstFragment* m_pFragment;
+  
+  /// m_Relocation - The list of relocations in this fragment
+  std::vector<LDRelocation> m_Relocations;
+  
+public:
+  typedef std::vector<LDRelocation>::const_iterator const_relocation_iterator;
+  typedef std::vector<LDRelocation>::iterator relocation_iterator;
+
+public:
+  MCInstFragment( llvm::MCInstFragment& pFragment )
+      : m_pFragment(&pFragment){
+    setParent( pFragment.getParent() );
+    setAtom( pFragment.getAtom() );
+    setLayoutOrder( pFragment.getLayoutOrder());
+  }
+  ~MCInstFragment(){}
+   
+  // ------ observers ------//
+  llvm::SmallVectorImpl<char> &getCode() { return m_pFragment->getCode(); }
+  const llvm::SmallVectorImpl<char> &getCode() const { return m_pFragment->getCode(); }
+
+  unsigned getInstSize() const { return m_pFragment->getCode().size(); }
+
+  llvm::MCInst &getInst() { return m_pFragment->getInst(); }
+  const llvm::MCInst &getInst() const { return m_pFragment->getInst(); }
+  
+  // ----- modifiers ------//
+  void setInst(llvm::MCInst pValue) { m_pFragment->setInst(pValue); }
+
+  // relocation access
+  void addRelocation(LDRelocation pReloc){  m_Relocations.push_back(pReloc); }
+  
+  std::vector<LDRelocation> &getRelocations() { return m_Relocations; }
+  const std::vector<LDRelocation> &getRelcoations() const { return m_Relocations; }
+
+  relocation_iterator relocation_begin() { return m_Relocations.begin(); }
+  const_relocation_iterator relocation_begin() const { return m_Relocations.begin(); }
+
+  relocation_iterator relocation_end() {return m_Relocations.end();}
+  const_relocation_iterator relocation_end() const {return m_Relocations.end();}
+
+  size_t relocations_size() const { return m_Relocations.size(); }
+
+  // fragment identification
+  static bool classof(const MCFragment *pF) {
+    return pF->getKind() == llvm::MCFragment::FT_Inst;
+  }
+  static bool classof(const MCInstFragment *) { return true; }
+  
+  // overwrite parent method
+  FragmentType getKind() const { return m_pFragment->getKind(); } 
+
+};
+
+} // namespace of mcld
+
+#endif
+
