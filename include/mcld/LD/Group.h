a472c0a9362ec080a26bd8ae04299ccea88f21c5
luba <lubatang@gmail.com>
2011-12-02 16:39:20 +0800

    Add empty class of Group and Layout
    Group and Layout are two classes used to finalize the layout of regions.

---
+++ include/mcld/LD/Group.h
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Robert Lai <rober.lai@mediatek.com>                                     *
+ *   Luba Tang <luba.tang@mediatek.com>                                      *
+ ****************************************************************************/
+#ifndef LD_GROUP_H
+#define LD_GROUP_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class Group
+ *  \brief Group records the grouping of all regions
+ */
+class Group
+{
+};
+
+} // namespace of mcld
+
+#endif
+
---
+++ include/mcld/LD/Layout.h
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Diana Chen <diana.chen@mediatek.com>                                    *
+ *   Chin-Yen Chou <chinyen.chou@mediatek.com>                               *
+ *   Luba Tang <luba.tang@mediatek.com>                                      *
+ ****************************************************************************/
+#ifndef LD_LAYOUT_H
+#define LD_LAYOUT_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class Layout
+ *  \brief Layout records the order and offset of all regions
+ */
+class Layout
+{
+};
+
+} // namespace of mcld
+
+#endif
+
