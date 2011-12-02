c2ebf70bccf6cc890f060e0445029d18b0e01260
luba <lubatang@gmail.com>
2011-12-02 16:31:12 +0800

    Add Howto
    add Howto as the common information of relocation entries

---
+++ lib/LD/Howto.cpp
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ *   The MCLinker Project, Copyright (C), 2011 -                             *
+ *   Embedded and Web Computing Lab, National Taiwan University              *
+ *   MediaTek, Inc.                                                          *
+ *                                                                           *
+ *   Diana Chen <diana.chen@mediatek.com>                                    *
+ ****************************************************************************/
+#include <mcld/LD/Howto.h>
+
+using namespace mcld;
+
+//==========================
+// Howto
+
+void Howto::apply(Relocation& pRel)
+{
+}
+
+
+Howto::Type Howto::type() const
+{
+}
+
+
