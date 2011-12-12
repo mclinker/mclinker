cd145bcdf7178b4a91a30ebc90579b26787156ff
luba <lubatang@gmail.com>
2011-12-12 21:35:28 +0800

    Layout laid out the sections and fragments

--- include/mcld/LD/Layout.h
+++ include/mcld/LD/Layout.h
@@ -6,8 +6,8 @@
 // License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
-#ifndef LD_LAYOUT_H
-#define LD_LAYOUT_H
+#ifndef MCLD_LAYOUT_H
+#define MCLD_LAYOUT_H
 #ifdef ENABLE_UNITTEST
 #include <gtest.h>
 #endif
@@ -20,6 +20,39 @@ namespace mcld
  */
 class Layout
 {
+public:
+  typedef std::vector<llvm::MCSectionData*> SectionOrder;
+  typedef SectionOrder::iterator sect_iterator;
+  typedef SectionOrder::const_iterator const_sect_iterator;
+
+public:
+  Layout();
+  ~Layout();
+
+  /// layoutFragment - perform layout for single fragment
+  /// Assuming the previous fragment has already been laid out correctly.
+  /// @return the offset of the file to laid out the fragment
+  uint64_t layoutFragment(MCFragment& pFrag);
+
+  // -----  modifiers  ----- //
+  bool layout(MCLinker& pLinker, LDFileFormat& pFormat);
+
+  // -----  iterators  ----- //
+  sect_iterator sect_begin()
+  { return m_SectionOrder.begin(); }
+
+  sect_iterator sect_end()
+  { return m_SectionOrder.end(); }
+
+  const_sect_iterator sect_begin() const
+  { return m_SectionOrder.begin(); }
+
+  const_sect_iterator sect_end() const
+  { return m_SectionOrder.end(); }
+
+private:
+  /// a vector to describe the order of sections
+  SectionOrder m_SectionOrder;
 };
 
 } // namespace of mcld
---
+++ lib/LD/Layout.cpp
@@ -0,0 +1,31 @@
+//===- Layout.cpp ---------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mcld/LD/Layout.h"
+#include "mcld/LD/LDFileFormat.h"
+#include "mcld/LD/MCLinker.h"
+
+using namespace mcld;
+
+Layout::Layout()
+{
+}
+
+Layout::~Layout()
+{
+}
+
+uint64_t Layout::layoutFragment(MCFragment& pFrag)
+{
+}
+
+bool Layout::layout(MCLinker& pLinker, LDFileFormat& pFormat)
+{
+}
+
