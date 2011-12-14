65f5588158b5d2b5c26e33b5977d1806c440d10f
Jush Lu <Jush.Lu@mediatek.com>
2011-12-14 10:59:02 +0800

    Fix building system.
    Fix the build failure when building MCLinker in separate building directory.

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 # This file is distributed under the University of Illinois Open Source
 # License. See LICENSE.TXT for details.
 
-aclocal -I ./m4
+aclocal -I ${MCLINKERTOP}/m4
 libtoolize --force
 automake --add-missing
 autoconf
