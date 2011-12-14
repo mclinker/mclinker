8eb2e5c8105f225941132c9fd51f1a9481cc2c7a
Simon Atanasyan <simon@atanasyan.com>
2011-12-14 12:16:49 +0400

    The autogen.sh do not use external env variable to get path to "m4" folder. It expects to see "m4" folder in the same directory where the autogen.sh is.

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,13 @@
+#!/bin/sh
 #                     The MCLinker project
 #
 # This file is distributed under the University of Illinois Open Source
 # License. See LICENSE.TXT for details.
 
-aclocal -I ${MCLINKERTOP}/m4
+root=`dirname $0`
+cd $root
+
+aclocal -I ./m4
 libtoolize --force
 automake --add-missing
 autoconf
-
