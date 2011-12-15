83c71bd31e8de366f55b6f1a00e7d946bbb1307c
Jush Lu <jush.msn@gmail.com>
2011-12-15 23:15:57 +0800

    Move ARM GOT and PLT entries to ARMLDBackend.h.

--- lib/Target/ARM/ARMLDBackend.cpp
+++ lib/Target/ARM/ARMLDBackend.cpp
@@ -8,8 +8,6 @@
 //===----------------------------------------------------------------------===//
 #include "ARM.h"
 #include "ARMLDBackend.h"
-#include "mcld/Target/GOT.h"
-#include "mcld/Target/PLT.h"
 #include "mcld/Support/TargetRegistry.h"
 #include "llvm/ADT/Triple.h"
 
@@ -36,74 +34,6 @@ uint32_t ARMGNULDBackend::machine() const
 }
 
 namespace mcld {
-
-static const uint32_t arm_plt0[] = {
-  0xe52de004, // str   lr, [sp, #-4]!
-  0xe59fe004, // ldr   lr, [pc, #4]
-  0xe08fe00e, // add   lr, pc, lr
-  0xe5bef008, // ldr   pc, [lr, #8]!
-  0x00000000, // &GOT[0] - .
-};
-
-static const uint32_t arm_plt1[] = {
-  0xe28fc600, // add   ip, pc, #0xNN00000
-  0xe28cca00, // add   ip, ip, #0xNN000
-  0xe5bcf000, // ldr   pc, [ip, #0xNNN]!
-};
-
-class ARMGOTEntry : public GOTEntry {
-public:
- ARMGOTEntry() : GOTEntry(sizeof(uint32_t), 0) {
-   initGOTEntry();
- }
-
- ~ARMGOTEntry() {
-   delete m_pContent;
-   m_pContent = 0;
- }
-
- void initGOTEntry() {
-   m_pContent = static_cast<unsigned char*>(malloc(m_EntrySize));
- }
-};
-
-class ARMPLT0 : public PLTEntry {
-public:
-  ARMPLT0(): PLTEntry(sizeof(arm_plt0), 0) {
-    initPLTEntry();
-  }
-
-  ~ARMPLT0() {
-    delete m_pContent;
-    m_pContent = 0;
-  }
-
-  void initPLTEntry() {
-    m_pContent = static_cast<unsigned char*>(malloc(m_EntrySize));
-
-    if (!m_pContent)
-      memcpy(m_pContent, arm_plt0, sizeof(arm_plt0));
-  }
-};
-
-class ARMPLT1 : public PLTEntry {
-public:
-  ARMPLT1() : PLTEntry(sizeof(arm_plt1),0) {
-    initPLTEntry();
-  };
-
-  ~ARMPLT1() {
-    delete m_pContent;
-    m_pContent = 0;
-  }
-
-  void initPLTEntry() {
-    m_pContent = static_cast<unsigned char*>(malloc(m_EntrySize));
-
-    if (!m_pContent)
-      memcpy(m_pContent, arm_plt1, sizeof(arm_plt1));
-  }
-};
 
 //===----------------------------------------------------------------------===//
 /// createARMLDBackend - the help funtion to create corresponding ARMLDBackend
