8c765870568f812c0e3d4ccc9fd6641230a1305b
luba <lubatang@gmail.com>
2011-12-15 14:45:35 +0800

    Fix the bugs in script builders
    Because we change our building system from BOLD (the old project name) to
    MCLinker (the new project name), so these scripts have to be changed consistently.

--- scripts/target_files.sh
+++ scripts/target_files.sh
@@ -8,8 +8,8 @@ function copy_template_header
 {
 	local NAME=$1
 	local DEPLOYMENT=$2
-	local TARGET_FILE="${BOLDTOP}/lib/${DEPLOYMENT}/${NAME}.h";
-	local SOURCE_FILE="${BOLDTOP}/templates/header.h";
+	local TARGET_FILE="${MCLINKERTOP}/lib/${DEPLOYMENT}/${NAME}.h";
+	local SOURCE_FILE="${MCLINKERTOP}/templates/header.h";
 	cp ${SOURCE_FILE} ${TARGET_FILE}
 	echo "${TARGET_FILE}";
 }
@@ -18,8 +18,8 @@ function copy_template_impl
 {
 	local NAME=$1
 	local DEPLOYMENT=$2
-	local TARGET_FILE="${BOLDTOP}/lib/${DEPLOYMENT}/${NAME}.cpp";
-	local SOURCE_FILE="${BOLDTOP}/templates/impl.cpp";
+	local TARGET_FILE="${MCLINKERTOP}/lib/${DEPLOYMENT}/${NAME}.cpp";
+	local SOURCE_FILE="${MCLINKERTOP}/templates/impl.cpp";
 	cp ${SOURCE_FILE} ${TARGET_FILE}
 	echo "${TARGET_FILE}";
 }
