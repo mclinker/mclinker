cda66ba64e9a763bab015ce23d0843e9459b3354
luba <lubatang@gmail.com>
2011-12-15 16:27:11 +0800

    Fix a bug
    ResolveInfoFactory produces the ResolveInfo with wrong types and bindings.

--- lib/LD/ResolveInfoFactory.cpp
+++ lib/LD/ResolveInfoFactory.cpp
@@ -25,7 +25,7 @@ ResolveInfoFactory::produce(const ResolveInfoFactory::key_type& pKey)
   new (result) entry_type();
   std::memcpy(result->m_Name, pKey.data(), pKey.size());
   result->m_Name[pKey.size()] = '\0';
-  result->m_BitField |= ResolveInfo::RESOLVE_MASK;
+  result->m_BitField &= ~ResolveInfo::RESOLVE_MASK;
   result->m_BitField |= (pKey.size() << ResolveInfo::NAME_LENGTH_OFFSET);
   return result;
 }
