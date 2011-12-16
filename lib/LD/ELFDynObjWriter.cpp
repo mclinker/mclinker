2b5181f9a8ce07e780a721cd7f47a4c0dcc58437
Jason Lin <sh.lin@mediatek.com>
2011-12-16 17:41:45 +0800

    Add 64bit object file output support

--- lib/LD/ELFDynObjWriter.cpp
+++ lib/LD/ELFDynObjWriter.cpp
@@ -15,10 +15,7 @@
 #include "mcld/Support/ScopedWriter.h"
 #include "llvm/Support/ELF.h"
 
-#include <iostream>
-
 using namespace mcld;
-using namespace std;
 
 //==========================
 // ELFDynObjWriter
@@ -41,20 +38,22 @@ bool ELFDynObjWriter::WriteObject()
 
 bool ELFDynObjWriter::WriteELFHeader()
 {
-  cout << "Write ELF Headers" << endl;
   MCLDInfo &ldInfo = m_Linker.getLDInfo();
   MCLDFile &ldFile = ldInfo.output();
   MemoryArea *memArea = ldFile.memArea();
 
-  ScopedWriter *pWriter = new ScopedWriter(memArea->request(0, sizeof(ELF::Elf32_Ehdr)), true);
+  bool is32BitClass = (32 == target().bitclass());
+
+  ScopedWriter *pWriter = new ScopedWriter(memArea->request(0,
+    (is32BitClass) ? sizeof(ELF::Elf32_Ehdr) : sizeof(ELF::Elf64_Ehdr)), true);
 
   /* ELF Identification */
   pWriter->Write8(ELF::ElfMagic[0]);   //EI_MAG0
   pWriter->Write8(ELF::ElfMagic[1]);   //EI_MAG1
   pWriter->Write8(ELF::ElfMagic[2]);   //EI_MAG2
   pWriter->Write8(ELF::ElfMagic[3]);   //EI_MAG3
-  pWriter->Write8(ELF::ELFCLASS32);    //EI_CLASS
-  pWriter->Write8(ELF::ELFDATA2LSB);   //EI_DATA
+  pWriter->Write8((is32BitClass) ? ELF::ELFCLASS32 : ELF::ELFCLASS64);     //EI_CLASS
+  pWriter->Write8(target().isLittleEndian() ? ELF::ELFDATA2LSB : ELF::ELFDATA2MSB); //EI_DATA
   pWriter->Write8(ELF::EV_CURRENT);    //EI_VERSION
   pWriter->Write8(0x0);                //EI_OSABI
   pWriter->Write8(0x0);                //EI_ABIVERSION
@@ -64,27 +63,36 @@ bool ELFDynObjWriter::WriteELFHeader()
   pWriter->Write16(ELF::ET_DYN);            //e_type
   pWriter->Write16(target().machine());     //e_machine
   pWriter->Write32(ELF::EV_CURRENT);        //e_version
-  //FIXME: it seems pointing to the first .text section
-  pWriter->Write32(0x0);                    //e_entry
+
   //FIXME:
-  pWriter->Write32(0x0);                    //e_phoff = program header offset
-  //FIXME:
-  pWriter->Write32(0x0);                    //e_shoff = section header offset FIXME
+  if (is32BitClass)
+  {
+    pWriter->Write32(0x0);                  //e_entry, it seems pointing to the first .text section
+    pWriter->Write32(0x0);                  //e_phoff = program header offset
+    pWriter->Write32(0x0);                  //e_shoff = section header offset FIXME
+  }else{
+    pWriter->Write64(0x0);
+    pWriter->Write64(0x0);
+    pWriter->Write64(0x0);
+  }
+
   //FIXME: ARM should add enrty point judgement
   if (ELF::EM_ARM == target().machine())    //e_flags
     pWriter->Write32(ELF::EF_ARM_EABIMASK & DefaultEABIVersion);
   else
     pWriter->Write32(0x0);
-  pWriter->Write16(sizeof(ELF::Elf32_Ehdr));     //e_ehsize = elf header size
+  pWriter->Write16((is32BitClass) ?
+                    sizeof(ELF::Elf32_Ehdr) : sizeof(ELF::Elf64_Ehdr)); //e_ehsize = elf header size
 
   //FIXME
   pWriter->Write16(0x0);                    //e_phentsize
   pWriter->Write16(0x0);                    //e_phnum
 
   //FIXME
-  pWriter->Write16(sizeof(ELF::Elf32_Shdr));  //e_shentsize
-  pWriter->Write16(0x0);                      //e_shnum
-  pWriter->Write16(ELF::SHN_UNDEF);           //e_shstrndx
+  pWriter->Write16((is32BitClass) ?
+                    sizeof(ELF::Elf32_Shdr) : sizeof(ELF::Elf64_Shdr)); //e_shentsize
+  pWriter->Write16(0x0);                    //e_shnum
+  pWriter->Write16(ELF::SHN_UNDEF);         //e_shstrndx
 
   delete pWriter;
 
