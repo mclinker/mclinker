cb8b34e4e3069323a97fdbcc9fc952baddccf829
Koan-Sin Tan <koansin.tan@gmail.com>
2011-12-17 10:25:31 +0800

    README: add source ./scripts/envsetup.sh autogen.sh: libtoolize is named glibtoolize on OS X

--- autogen.sh
+++ autogen.sh
@@ -4,10 +4,16 @@
 # This file is distributed under the University of Illinois Open Source
 # License. See LICENSE.TXT for details.
 
+LIBTOOLIZE=libtoolize
+my_uname=`uname`
+
+if [ ${my_uname} == "Darwin" ]; then
+  LIBTOOLIZE=glibtoolize
+fi
 root=`dirname $0`
 cd $root
 
 aclocal -I ./m4
-libtoolize --force
+${LIBTOOLIZE} --force
 automake --add-missing
 autoconf
