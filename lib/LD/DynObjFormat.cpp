b7d7c1a757de72a7691effe57a3b80f4c85bca28
Chinyen Chou <petechou@gmail.com>
2011-12-18 16:25:34 +0800

    Add empty class for DynObjFormat and ELFDynObjFormat.

---
+++ lib/LD/DynObjFormat.cpp
@@ -0,0 +1,15 @@
+//===- impl.cpp -----------------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include <mcld/LD/DynObjFormat.h>
+
+using namespace mcld;
+
+//==========================
+// DynObjFormat
+
