bfadd67b5dd4adf7c627c5c4ffc7cbda8b8f835c
Diana Chen <diana.chen@mediatek.com>
2011-12-23 21:20:14 +0800

    Create ARMDynRelSection
    The dynamic relocation table for .rel.plt and .rel.dyn sectoin

---
+++ lib/Target/ARM/ARMDynRelSection.cpp
@@ -0,0 +1,55 @@
+//===- ARMDynRelSection.cpp ------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include "ARMDynRelSection.h"
+
+using namespace mcld;
+
+//==========================
+// ARMDynRelSection
+
+
+ARMDynRelSection::ARMDynRelSection(llvm::MCSectionData& pSectionData)
+                                   : m_pSectionData(&pSectionData)
+{
+}
+
+ARMDynRelSection::~ARMDynRelSection()
+{
+}
+
+void ARMDynRelSection::reserveEntry(RelocationFactory& pRelFactory,
+                                    int pNum)
+{
+  m_pSectionData->getFragmentList().push_back(pRelFactory.produceEmptyEntry());
+}
+
+Relocation* ARMDynRelSection::getEntry(const ResolveInfo& pSymbol,
+                                        bool& pExist)
+{
+  // first time visit this function, set m_pEmpty to Fragments.begin()
+  if(m_SymRelMap.empty()) {
+      assert( !m_pSectionData->getFragmentList().empty() &&
+             "DynRelSection contains no entries.");
+    m_pEmpty = m_pSectionData->getFragmentList().begin();
+  }
+
+  // get or create entry in m_SymRelMap
+  Relocation *&Entry = m_SymRelMap[&pSymbol];
+  pExist = 1;
+
+  if(!Entry) {
+    pExist = 0;
+    assert(m_pEmpty != m_pSectionData->end() &&
+           "No empty relocation entry for the incoming symbol.");
+    Entry = llvm::cast<Relocation>(&(*m_pEmpty));
+    ++m_pEmpty;
+  }
+
+  return Entry;
+}
