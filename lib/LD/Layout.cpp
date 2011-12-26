9d346b4d9295b9e369a7530fad86fa972459307e
Chinyen Chou <petechou@gmail.com>
2011-12-26 17:04:41 +0800

    Implement addInputRange in Layout.

--- lib/LD/Layout.cpp
+++ lib/LD/Layout.cpp
@@ -7,6 +7,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <llvm/ADT/Twine.h>
+#include <llvm/Support/ErrorHandling.h>
 #include <mcld/LD/Layout.h>
 #include <mcld/LD/LDFileFormat.h>
 #include <mcld/MC/MCLinker.h>
@@ -48,5 +50,32 @@ MCFragmentRef Layout::getFragmentRef(const LDSection& pInputSection,
 
 void Layout::addInputRange(const llvm::MCSectionData& pSD, const LDSection& pInputHdr)
 {
+  // check if mapping the input range to a existing SectionData or a new one
+  InputRangeList::iterator sect_data_iter = m_InputRangeList.find(&pSD);
+  RangeList* sect_data;
+  if (sect_data_iter != m_InputRangeList.end()) {
+    sect_data = sect_data_iter->second;
+    assert(NULL != sect_data);
+    // check if we already built a mapping for the same input section header
+    RangeList::iterator it;
+    for (it = sect_data->begin(); it != sect_data->end(); ++it) {
+      if (&pInputHdr == &(*(it->header)))
+        llvm::report_fatal_error(llvm::Twine("Trying to map second range of ") +
+                                 pInputHdr.name() +
+                                 llvm::Twine(" into the same SectionData!\n"));
+    }
+  } else {
+    sect_data = m_InputRangeList[&pSD] = new RangeList();
+    assert(NULL != sect_data);
+  }
+
+  Range range = {
+    &pInputHdr,
+    NULL,
+  };
+  if (!pSD.getFragmentList().empty())
+    range.prevRear =
+      const_cast<llvm::MCFragment*>(&pSD.getFragmentList().back());
+  sect_data->push_back(range);
 }
 
