a11f8bcd3be88f45629d9ce4b83849de7f086446
Simon Atanasyan <simon@atanasyan.com>
2012-01-02 14:02:34 +0400

    Refine MipsGOT code.

--- lib/Target/Mips/MipsGOT.cpp
+++ lib/Target/Mips/MipsGOT.cpp
@@ -16,15 +16,11 @@ MipsGOT::MipsGOT(LDSection& pSection, llvm::MCSectionData& pSectionData)
   : GOT(pSection, pSectionData, 4 /* Mips uses 32-bit GOT entry */) {
 }
 
-MipsGOT::~MipsGOT()
-{
-}
-
 void MipsGOT::reserveEntry(int pNum)
 {
 }
 
-MipsGOT::Entry* MipsGOT::getEntry(const ResolveInfo& pSymbol, bool& pExist)
+GOTEntry* MipsGOT::getEntry(const ResolveInfo& pSymbol, bool& pExist)
 {
   return 0;
 }
--- lib/Target/Mips/MipsGOT.h
+++ lib/Target/Mips/MipsGOT.h
@@ -25,12 +25,6 @@ class LDSection;
 class MipsGOT : public GOT
 {
 public:
-  typedef GOTEntry Entry;
-
-public:
-  // for target dependent section, target should maintain its Section size
-  // in LDSection by target itself, so we let GOT holds a pointer to its
-  // LDSection
   MipsGOT(LDSection& pSection, llvm::MCSectionData& pSectionData);
   ~MipsGOT();
 
@@ -38,7 +32,7 @@ public:
   // layout will get the correct size of GOT section
   void reserveEntry(int pNum = 1);
 
-  Entry* getEntry(const ResolveInfo& pSymbol, bool& pExist);
+  GOTEntry* getEntry(const ResolveInfo& pSymbol, bool& pExist);
 };
 
 } // namespace of mcld
