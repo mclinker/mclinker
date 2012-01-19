ed1c3740140cd1f0312becc1104450affb80854b
H.J. Lu <hjl.tools@gmail.com>
2012-01-19 12:31:54 -0800

    Add X86DynRelSection
    It is copied from ARMDynRelSection.

---
+++ lib/Target/X86/X86DynRelSection.cpp
@@ -0,0 +1,80 @@
+//===- X86DynRelSection.cpp ------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include <mcld/LD/LDSection.h>
+#include "X86DynRelSection.h"
+
+using namespace mcld;
+
+//==========================
+// X86DynRelSection
+
+
+X86DynRelSection::X86DynRelSection(LDSection& pSection,
+                                   llvm::MCSectionData& pSectionData,
+                                   const unsigned int pEntrySize)
+  : m_pSection(&pSection),
+    m_pSectionData(&pSectionData),
+    m_EntryBytes(pEntrySize),
+    m_pEmpty(0){
+}
+
+X86DynRelSection::~X86DynRelSection()
+{
+}
+
+void X86DynRelSection::reserveEntry(RelocationFactory& pRelFactory,
+                                    int pNum)
+{
+  for(int i=0; i<pNum; i++) {
+    m_pSectionData->getFragmentList().push_back(pRelFactory.produceEmptyEntry());
+    // update section size
+    m_pSection->setSize(m_pSection->size() + m_EntryBytes);
+  }
+}
+
+Relocation* X86DynRelSection::getEntry(const ResolveInfo& pSymbol,
+                                       bool isForGOT,
+                                       bool& pExist)
+{
+  // first time visit this function, set m_pEmpty to Fragments.begin()
+  if(!m_pEmpty) {
+    assert( !m_pSectionData->getFragmentList().empty() &&
+             "DynRelSection contains no entries.");
+    m_pEmpty = m_pSectionData->getFragmentList().begin();
+  }
+
+  assert(m_pEmpty != m_pSectionData->end() &&
+         "No empty relocation entry for the incoming symbol.");
+
+  // if this relocation is used to relocate GOT (.got or .got.plt),
+  // check if we've gotten an entry for this symbol before. If yes,
+  // return the found entry in map.
+  // Otherwise, this relocation is used to relocate general section
+  // (data or text section), return an empty entry directly.
+  Relocation* result;
+
+  if(isForGOT) {
+    // get or create entry in m_SymRelMap
+    Relocation *&Entry = m_SymRelMap[&pSymbol];
+    pExist = 1;
+
+    if(!Entry) {
+      pExist = 0;
+      Entry = llvm::cast<Relocation>(&(*m_pEmpty));
+      ++m_pEmpty;
+    }
+    result = Entry;
+  }
+  else {
+    pExist = 0;
+    result = llvm::cast<Relocation>(&(*m_pEmpty));
+    ++m_pEmpty;
+  }
+  return result;
+}
