e088bd0415d86efd20d01ca5c43f3e964c4325e8
H.J. Lu <hjl.tools@gmail.com>
2012-01-20 09:13:43 -0800

    Properly handle R_386_PLT32 without PLT entry

--- lib/Target/X86/X86RelocationFactory.cpp
+++ lib/Target/X86/X86RelocationFactory.cpp
@@ -346,10 +346,12 @@ X86RelocationFactory::Result plt32(Relocation& pReloc,
 				   const MCLDInfo& pLDInfo,
                                    X86RelocationFactory& pParent)
 {
-  if(!(pReloc.symInfo()->reserved() & X86GNULDBackend::ReservePLT)) {
-    return X86RelocationFactory::BadReloc;
-  }
-  X86RelocationFactory::Address PLT_S = helper_PLT(pReloc, pParent);
+  // PLT_S depends on if there is a PLT entry.
+  X86RelocationFactory::Address PLT_S;
+  if((pReloc.symInfo()->reserved() & X86GNULDBackend::ReservePLT))
+    PLT_S = helper_PLT(pReloc, pParent);
+  else
+    PLT_S = pReloc.symValue();
   RelocationFactory::DWord   A       = pReloc.target() + pReloc.addend();
   X86RelocationFactory::Address P = pReloc.place(pParent.getLayout());
   pReloc.target() = PLT_S + A - P;
