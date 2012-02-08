8ff5d1cdeed63bb3261c9b83f1535e4ff0577cfb
Jush Lu <Jush.Lu@mediatek.com>
2012-02-08 17:35:08 +0800

    Add a regression test for Plasma on ARM.
    Currently running the regression test for Plasma by binary file comparison.
    Remove it when MCLinker has more test cases to ensure that Plasma will work.

---
+++ test/Android/Plasma/ARM/plasma_arm.ts
@@ -0,0 +1,21 @@
+: RUN: cp %p/../plasma/libplasma_portable.bc ../
+
+; Build the object file.
+; RUN: %MCLinker -filetype=obj -relocation-model=pic \
+; RUN: -mtriple="armv7-none-linux-gnueabi" \
+; RUN: -dB ../libplasma_portable.bc -o Output/libplasma_portable.o
+
+; Build the shared library.
+; RUN: %MCLinker -filetype=dso -march=arm -soname=libplasma_portable.so \
+; RUN: -L=%p/../../../libs/ARM/Android/android-14  -Bsymbolic \
+; RUN: %p/../../../libs/ARM/Android/android-14/crtbegin_so.o \
+; RUN: %p/../../../libs/ARM/Android/android-14/crtend_so.o \
+; RUN: Output/libplasma_portable.o \
+; RUN: -dB ../libplasma_portable.bc -o Output/libplasma_portable.so
+
+; RUN: diff -s Output/libplasma_portable.so %p/../plasma/golden/ARM/libplasma_portable.so | \
+; RUN: awk '{print $6}' | \
+; RUN: FileCheck %s -check-prefix=Plasma
+
+; Plasma: identical
+
