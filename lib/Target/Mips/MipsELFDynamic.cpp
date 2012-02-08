9dfd8cc0712d502b90557c1f3a9d30e7a73e2319
Simon Atanasyan <simon@atanasyan.com>
2012-02-08 14:00:00 +0400

    Reserver and apply MIPS specific dynamic section entries.

--- lib/Target/Mips/MipsELFDynamic.cpp
+++ lib/Target/Mips/MipsELFDynamic.cpp
@@ -7,11 +7,20 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <llvm/Support/ErrorHandling.h>
-
+#include <mcld/LD/ELFFileFormat.h>
 #include "MipsELFDynamic.h"
 
 using namespace mcld;
+
+// MIPS mandatory dynamic section entries
+enum {
+  MIPS_RLD_VERSION  = 0x70000001,
+  MIPS_FLAGS        = 0x70000005,
+  MIPS_BASE_ADDRESS = 0x70000006,
+  MIPS_LOCAL_GOTNO  = 0x7000000a,
+  MIPS_SYMTABNO     = 0x70000011,
+  MIPS_GOTSYM       = 0x70000013,
+};
 
 MipsELFDynamic::MipsELFDynamic(const GNULDBackend& pParent)
   : ELFDynamic(pParent)
@@ -24,14 +33,29 @@ MipsELFDynamic::~MipsELFDynamic()
 
 void MipsELFDynamic::reserveTargetEntries(const ELFFileFormat& pFormat)
 {
-  // FIXME: unsupport reserving DT_PLTGOT entry for Mips
-  llvm::report_fatal_error("unsupport reserving DT_PLTGOT entry for Mips!");
+  // reservePLTGOT
+  if (pFormat.hasGOT())
+    reserveOne(llvm::ELF::DT_PLTGOT);
+
+  reserveOne(MIPS_RLD_VERSION);
+  reserveOne(MIPS_FLAGS);
+  reserveOne(MIPS_BASE_ADDRESS);
+  reserveOne(MIPS_LOCAL_GOTNO);
+  reserveOne(MIPS_SYMTABNO);
+  reserveOne(MIPS_GOTSYM);
 }
 
 void MipsELFDynamic::applyTargetEntries(const ELFFileFormat& pFormat)
 {
-  // FIXME: unsupport applying value to DT_PLTGOT entry for Mips
-  llvm::report_fatal_error(
-    "unsupport applying value to DT_PLTGOT entry for Mips!");
+  // applyPLTGOT
+  if (pFormat.hasGOT())
+    applyOne(llvm::ELF::DT_PLTGOT, pFormat.getGOT().addr());
+
+  applyOne(MIPS_RLD_VERSION, 1);
+  applyOne(MIPS_FLAGS, 0);
+  applyOne(MIPS_BASE_ADDRESS, 0);
+  applyOne(MIPS_LOCAL_GOTNO, 0);
+  applyOne(MIPS_SYMTABNO, size());
+  applyOne(MIPS_GOTSYM, 0);
 }
 
