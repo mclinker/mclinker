62ab473c9f6365ffd9378b3d6d4c6c627a07fbb7
Simon Atanasyan <simon@atanasyan.com>
2012-02-08 14:00:00 +0400

    Refine reservation type constant values.

--- lib/Target/Mips/MipsLDBackend.h
+++ lib/Target/Mips/MipsLDBackend.h
@@ -25,10 +25,10 @@ class MipsGNULDBackend : public GNULDBackend
 {
 public:
   enum ReservedEntryType {
-    None          =  0, // no reserved entry
-    ReserveRel    =  1, // reserve a dynamic relocation entry
-    ReserveGot    =  2, // reserve a GOT entry
-    ReserveGpDisp = 32  // reserve _gp_disp symbol
+    None          = 0,  // no reserved entry
+    ReserveRel    = 1,  // reserve a dynamic relocation entry
+    ReserveGot    = 2,  // reserve a GOT entry
+    ReserveGpDisp = 8   // reserve _gp_disp symbol
   };
 
 public:
