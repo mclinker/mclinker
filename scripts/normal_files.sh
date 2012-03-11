d5decba261d3ad72d0bb978a805e3269a79bd014
Luba Tang <lubatang@gmail.com>
2012-03-11 16:00:05 +0800

    Conforming to FreeBSD's sed.

--- scripts/normal_files.sh
+++ scripts/normal_files.sh
@@ -23,24 +23,24 @@ function copy_template_header
 	local SOURCE_FILE="${MCLINKERTOP}/templates/header.h";
 	mkdir -p ${MCLINKERTOP}/include/mcld/${DEPLOYMENT}
 	cp ${SOURCE_FILE} ${TARGET_FILE}
-    # replace the template name by target flie name and adjust the padding of '-'
-    PADDING_LEN=`expr length header - length ${NAME}`;
-    ABS_PADDING_LEN=${PADDING_LEN};
-    PADDING="";
-    if [ "${PADDING_LEN}" -lt "0" ]; then
-        ABS_PADDING_LEN=`expr ${ABS_PADDING_LEN} \* -1`;
-    fi
-    for (( i=0; i<${ABS_PADDING_LEN}; i++ ));
-    do
-        PADDING="${PADDING}-";
-    done
-    if [ "${PADDING_LEN}" -gt "0" ]; then # replace and add padding
-        sed -i "s/header.h /${NAME}.h ${PADDING}/g" ${TARGET_FILE}
-    elif [ "${PADDING_LEN}" -lt "0" ]; then # replace and strip padding
-        sed -i "s/header.h ${PADDING}/${NAME}.h /g" ${TARGET_FILE}
-    else # replace directly
-        sed -i "s/header/${NAME}/g" ${TARGET_FILE}
-    fi
+	# replace the template name by target flie name and adjust the padding of '-'
+	local header="header";
+	PADDING_LEN=`expr ${#header} - ${#NAME}`;
+	ABS_PADDING_LEN=${PADDING_LEN};
+	PADDING="";
+	if [ ${PADDING_LEN} -lt 0 ]; then
+		ABS_PADDING_LEN=`expr ${ABS_PADDING_LEN} \* -1`;
+	fi
+	for (( i=0; i < ${ABS_PADDING_LEN}; i = i + 1 )); do
+		PADDING="${PADDING}-";
+	done
+	if [ ${PADDING_LEN} -gt 0 ]; then # replace and add padding
+		sed -e "s/header.h /${NAME}.h ${PADDING}/g" ${SOURCE_FILE} > ${TARGET_FILE}
+	elif [ ${PADDING_LEN} -lt 0 ]; then # replace and strip padding
+		sed -e "s/header.h ${PADDING}/${NAME}.h /g" ${SOURCE_FILE} > ${TARGET_FILE}
+	else # replace directly
+		sed -e "s/header/${NAME}/g" ${SOURCE_FILE} > ${TARGET_FILE}
+	fi
 	echo "${TARGET_FILE}";
 }
 
@@ -52,24 +52,25 @@ function copy_template_impl
 	local SOURCE_FILE="${MCLINKERTOP}/templates/impl.cpp";
 	mkdir -p ${MCLINKERTOP}/lib/${DEPLOYMENT}
 	cp ${SOURCE_FILE} ${TARGET_FILE}
-    # replace the template name by target flie name and adjust the padding of '-'
-    PADDING_LEN=`expr length impl - length ${NAME}`;
-    ABS_PADDING_LEN=${PADDING_LEN};
-    PADDING="";
-    if [ "${PADDING_LEN}" -lt "0" ]; then
-        ABS_PADDING_LEN=`expr ${ABS_PADDING_LEN} \* -1`;
-    fi
-    for (( i=0; i<${ABS_PADDING_LEN}; i++ ));
-    do
-        PADDING="${PADDING}-";
-    done
-    if [ "${PADDING_LEN}" -gt "0" ]; then # replace and add padding
-        sed -i "s/impl.cpp /${NAME}.cpp ${PADDING}/g" ${TARGET_FILE}
-    elif [ "${PADDING_LEN}" -lt "0" ]; then # replace and strip padding
-        sed -i "s/impl.cpp ${PADDING}/${NAME}.cpp /g" ${TARGET_FILE}
-    else # replace directly
-        sed -i "s/impl/${NAME}/g" ${TARGET_FILE}
-    fi
+	# replace the template name by target flie name and adjust the padding of '-'
+	local impl="impl";
+	PADDING_LEN=`expr ${#impl} - ${#NAME}`;
+	ABS_PADDING_LEN=${PADDING_LEN};
+	PADDING="";
+	if [ ${PADDING_LEN} -lt 0 ]; then
+		ABS_PADDING_LEN=`expr ${ABS_PADDING_LEN} \* -1`;
+	fi
+	for (( i=0; i < ${ABS_PADDING_LEN}; i = i + 1 )); do
+		PADDING="${PADDING}-";
+	done
+
+	if [ ${PADDING_LEN} -gt 0 ]; then # replace and add padding
+		sed -e "s/impl.cpp /${NAME}.cpp ${PADDING}/g" ${SOURCE_FILE} > ${TARGET_FILE}
+	elif [ "${PADDING_LEN}" -lt "0" ]; then # replace and strip padding
+		sed -e "s/impl.cpp ${PADDING}/${NAME}.cpp /g" ${SOURCE_FILE} > ${TARGET_FILE}
+	else # replace directly
+		sed -e "s/impl/${NAME}/g" ${SOURCE_FILE} > ${TARGET_FILE}
+	fi
 
 	echo "${TARGET_FILE}";
 }
@@ -80,14 +81,14 @@ function replace_author
 	shift
 	local AUTHOR=$*
 
-	sed -i "s/\${AUTHOR}/${AUTHOR}/g" ${TARGET_FILE}
+	sed -i "" "s/\${AUTHOR}/${AUTHOR}/g" ${TARGET_FILE}
 }
 
 function repalce_email
 {
 	local TARGET_FILE=$1
 	local EMAIL=$2
-	sed -i "s/\${EMAIL}/${EMAIL}/g" ${TARGET_FILE}
+	sed -i "" "s/\${EMAIL}/${EMAIL}/g" ${TARGET_FILE}
 
 	# adjust the length of header
 	NEW_SPACE=`grep '>                                                    *' ${TARGET_FILE} | wc -c`
@@ -97,12 +98,12 @@ function repalce_email
 		for (( i=0; i<${NEW_SPACE}; i=i+1 )); do
 			ADDEND="${ADDEND} ";
 		done
-		sed -i "s/${EMAIL}>/${EMAIL}${ADDEND}/g" ${TARGET_FILE}
+		sed -i "" "s/${EMAIL}>/${EMAIL}${ADDEND}/g" ${TARGET_FILE}
 	elif [ "${NEW_SPACE}" -lt "0" ]; then # strip space
 		for (( i=${NEW_SPACE}; i<0; i=i+1 )); do
 			ADDEND="${ADDEND} ";
 		done
-		sed -i "s/${EMAIL}${ADDEND}/${EMAIL}>/g" ${TARGET_FILE}
+		sed -i "" "s/${EMAIL}${ADDEND}/${EMAIL}>/g" ${TARGET_FILE}
 	fi
 }
 
@@ -112,8 +113,8 @@ function replace_class
 	local CLASS_NAME=$2
 	local UPCLASS_NAME=`echo ${CLASS_NAME} | tr [:lower:] [:upper:]`
 
-	sed -i "s/\${class_name}/${CLASS_NAME}/g" ${TARGET_FILE}
-	sed -i "s/\${CLASS_NAME}/${UPCLASS_NAME}/g" ${TARGET_FILE}
+	sed -i "" "s/\${class_name}/${CLASS_NAME}/g" ${TARGET_FILE}
+	sed -i "" "s/\${CLASS_NAME}/${UPCLASS_NAME}/g" ${TARGET_FILE}
 }
 
 function replace_brief
@@ -121,6 +122,6 @@ function replace_brief
 	local TARGET_FILE=$1
 	shift
 	local BRIEF=$*
-	sed -i "s/\${brief}/${BRIEF}/g" ${TARGET_FILE}
+	sed -i "" "s/\${brief}/${BRIEF}/g" ${TARGET_FILE}
 }
 
