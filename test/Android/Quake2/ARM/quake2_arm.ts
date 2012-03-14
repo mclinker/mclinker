99a16a23c03e34e50537efe5337fc2b00444edce
Jush Lu <Jush.Lu@mediatek.com>
2012-03-14 17:44:15 +0800

    Update golden model files for X86.

--- test/Android/Quake2/ARM/quake2_arm.ts
+++ test/Android/Quake2/ARM/quake2_arm.ts
@@ -1,18 +1,18 @@
-: RUN: cp %p/../libquake2.bc ./
+: RUN: cp %p/../libquake2_portable.bc ./
 
 ; Build the object file.
 ; RUN: %MCLinker -filetype=obj -relocation-model=pic \
 ; RUN: -mtriple="armv7-none-linux-gnueabi" \
-; RUN: -dB libquake2.bc -o libquake2.o
+; RUN: -dB libquake2_portable.bc -o libquake2_portable.o
 
 ; Build the shared library.
-; RUN: %MCLinker -filetype=dso -march=arm -soname=libquake2.so \
+; RUN: %MCLinker -filetype=dso -march=arm -soname=libquake2_portable.so \
 ; RUN: -L=%p/../../../libs/ARM/Android/android-14  -Bsymbolic \
 ; RUN: %p/../../../libs/ARM/Android/android-14/crtbegin_so.o \
 ; RUN: %p/../../../libs/ARM/Android/android-14/crtend_so.o \
-; RUN: libquake2.o \
-; RUN: -o libquake2.so \
+; RUN: libquake2_portable.o \
+; RUN: -o libquake2_portable.so \
 ; RUN: -lGLESv1_CM -llog -lc -lm
 
-; RUN: diff -s libquake2.so %p/libquake2.golden.so
+; RUN: diff -s libquake2_portable.so %p/libquake2_portable.golden.so
 
--- test/Android/Quake2/X86/quake2_x86.ts
+++ test/Android/Quake2/X86/quake2_x86.ts
@@ -1,15 +1,15 @@
-: RUN: cp %p/../libquake2.bc ./
+: RUN: cp %p/../libquake2_portable.bc ./
 
 ; Build the object file.
 ; RUN: %MCLinker -filetype=obj -relocation-model=pic -march=x86 \
-; RUN: -dB libquake2.bc -o libquake2.o
+; RUN: -dB libquake2_portable.bc -o libquake2_portable.o
 
 ; Build the shared library.
-; RUN: %MCLinker -filetype=dso -march=x86 -soname=libquake2.so \
+; RUN: %MCLinker -filetype=dso -march=x86 -soname=libquake2_portable.so \
 ; RUN: -L=%p/../../../libs/X86/Android/android-14  -Bsymbolic \
-; RUN: libquake2.o \
-; RUN: -o libquake2.so \
+; RUN: libquake2_portable.o \
+; RUN: -o libquake2_portable.so \
 ; RUN: -llog -lEGL -lGLESv1_CM -lOpenSLES -lc -lm
 
-; RUN: diff -s libquake2.so %p/libquake2.golden.so
+; RUN: diff -s libquake2_portable.so %p/libquake2_portable.golden.so
 
