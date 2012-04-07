ef31d3a17831b9f8a10a835088adf24e2b2f2fd8
Luba Tang <lubatang@gmail.com>
2012-04-08 02:53:23 +0800

    Add DumbDiagnostic

---
+++ lib/LD/DumbDiagnostic.cpp
@@ -0,0 +1,15 @@
+//===- DumbDiagnostic.cpp -------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include <mcld/LD/DumbDiagnostic.h>
+
+using namespace mcld;
+
+//==========================
+// DumbDiagnostic
+
