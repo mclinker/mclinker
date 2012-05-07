d943c90acf631ac1c5c800d4949a0ef157597bba
Luba Tang <lubatang@gmail.com>
2012-05-07 21:38:35 +0800

    add DiagnosticPrinter
    DiagnosticPrinter controls the output formats.

---
+++ include/mcld/LD/DiagnosticPrinter.h
@@ -0,0 +1,29 @@
+//===- DiagnosticPrinter.h ------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef MCLD_DIAGNOSTIC_PRINTER_H
+#define MCLD_DIAGNOSTIC_PRINTER_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class DiagnosticPrinter
+ *  \brief DiagnosticPrinter provides the interface to customize diagnostic messages and output.
+ */
+class DiagnosticPrinter
+{
+
+};
+
+} // namespace of mcld
+
+#endif
+
