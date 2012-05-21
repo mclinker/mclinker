e89256d5b2841a707c67e7e662f4f34d66fa6a50
Luba Tang <lubatang@gmail.com>
2012-05-21 15:01:49 +0800

    Sorry, I forget to push the implementation of previous commit.

--- lib/LD/MsgHandler.cpp
+++ lib/LD/MsgHandler.cpp
@@ -34,6 +34,14 @@ void MsgHandler::addString(llvm::StringRef pStr) const
   m_Engine.state().ArgumentStrs[m_NumArgs++] = pStr.data();
 }
 
+void MsgHandler::addString(const std::string& pStr) const
+{
+  assert(m_NumArgs < DiagnosticEngine::MaxArguments &&
+         "Too many arguments to diagnostic!");
+  m_Engine.state().ArgumentKinds[m_NumArgs] = DiagnosticEngine::ak_std_string;
+  m_Engine.state().ArgumentStrs[m_NumArgs++] = pStr;
+}
+
 void MsgHandler::addTaggedVal(intptr_t pValue, DiagnosticEngine::ArgumentKind pKind) const
 {
   assert(m_NumArgs < DiagnosticEngine::MaxArguments &&
