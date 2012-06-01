afbf7c17361e12db60f82f566a69328622ed6cc1
Luba Tang <lubatang@gmail.com>
2012-06-01 16:31:09 +0800

    Fix a bug - sorry, I forgot to push EhFrameHdr into main stream.

---
+++ include/mcld/LD/EhFrameHdr.h
@@ -0,0 +1,29 @@
+//===- EhFrameHdr.h -------------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef MCLD_EHFRAMEHDR_H
+#define MCLD_EHFRAMEHDR_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class EhFrameHdr
+ *  \brief EhFrameHdr represents .eh_frame_hdr section.
+ */
+class EhFrameHdr
+{
+
+};
+
+} // namespace of mcld
+
+#endif
+
---
+++ lib/LD/EhFrameHdr.cpp
@@ -0,0 +1,15 @@
+//===- EhFrameHdr.cpp -----------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include <mcld/LD/EhFrameHdr.h>
+
+using namespace mcld;
+
+//==========================
+// EhFrameHdr
+
