6cc1b71eb5a5fc2f3746f080cc03cbf12eed27a9
Diana Chen <diana.chen@mediatek.com>
2012-06-08 17:10:43 +0800

    Add some basic functions for EhFrame
    EhFrame::readFrame -
    1. parse the .eh_frame section and create the RegionFragments
       for each CIE and FDE.
    2. Call readCIE() or readFDE() to parse and create the CIE or
       FDE entry.

--- include/mcld/LD/EhFrame.h
+++ include/mcld/LD/EhFrame.h
@@ -13,21 +13,46 @@
 #endif
 #include <vector>
 
+#include <mcld/ADT/TypeTraits.h>
+#include <mcld/LD/CIE.h>
+#include <mcld/LD/FDE.h>
+#include <mcld/MC/MCRegionFragment.h>
+#include <mcld/Support/GCFactory.h>
+
 namespace mcld
 {
-
-class FDE;
-class CIE;
 /** \class EhFrame
  *  \brief EhFrame represents .eh_frame section
  */
 class EhFrame
 {
 public:
+  typedef ConstTraits<unsigned char>::pointer ConstAddress;
+
+public:
   EhFrame();
   ~EhFrame();
 
+  // readEhFrame - read an .eh_frame section and create the corresponding
+  // CIEs and FDEs
+  bool readEhFrame(MCLinker* pLinker,
+                   const LDSection& pSection,
+                   MemoryArea& pArea);
+
 private:
+  bool readCIE(const MCRegionFragment& pFrag, bool pIsTargetLittleEndian);
+  bool readFDE(const MCRegionFragment& pFrag, bool pIsTargetLittleEndian);
+
+  // readVal - read a 32 bit data from pAddr, swap if needed
+  uint32_t readVal(ConstAddress pAddr, bool pIsTargetLittleEndian);
+
+private:
+  typedef GCFactory<CIE, 0> CIEFactory;
+  typedef GCFactory<FDE, 0> FDEFactory;
+
+private:
+  CIEFactory m_CIEFactory;
+  FDEFactory m_FDEFactory;
   std::vector<FDE*> m_FDEs;
   std::vector<CIE*> m_CIEs;
 };
--- lib/LD/EhFrame.cpp
+++ lib/LD/EhFrame.cpp
@@ -13,10 +13,32 @@ using namespace mcld;
 //==========================
 // EhFrame
 EhFrame::EhFrame()
-{
+ : m_CIEFactory(32), m_FDEFactory(32) {
 }
 
 EhFrame::~EhFrame()
 {
 }
 
+bool EhFrame::readEhFrame(MCLinker* pLinker,
+                          const LDSection& pSection,
+                          MemoryArea& pArea)
+{
+  return true;
+}
+
+
+bool EhFrame::readCIE(const MCRegionFragment& pFrag, bool pIsTargetLittleEndian)
+{
+  return true;
+}
+
+bool EhFrame::readFDE(const MCRegionFragment& pFrag, bool pIsTargetLittleEndian)
+{
+  return true;
+}
+
+uint32_t EhFrame::readVal(ConstAddress pAddr, bool pIsTargetLittleEndian)
+{
+  return 0;
+}
