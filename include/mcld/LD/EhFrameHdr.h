97d49b6cc85d8b47ed0c4df890dfe81c6c2913c8
Chinyen Chou <petechou@gmail.com>
2012-06-20 14:17:53 +0800

    Add EhFrameHdr implementation

--- include/mcld/LD/EhFrameHdr.h
+++ include/mcld/LD/EhFrameHdr.h
@@ -11,17 +11,87 @@
 #ifdef ENABLE_UNITTEST
 #include <gtest.h>
 #endif
+#include <llvm/Support/Dwarf.h>
+#include <llvm/Support/DataTypes.h>
+#include <mcld/ADT/SizeTraits.h>
+#include <mcld/Support/MemoryArea.h>
+#include <mcld/Support/MemoryRegion.h>
+#include <mcld/MC/MCLDOutput.h>
+#include <mcld/MC/MCLinker.h>
+#include <mcld/LD/EhFrame.h>
+#include <mcld/LD/LDSection.h>
+#include <mcld/LD/CIE.h>
+#include <mcld/LD/FDE.h>
+#include <mcld/LD/Layout.h>
 
 namespace mcld
 {
+class EhFrame;
+class LDSection;
+class Output;
+class FDE;
+class MCLinker;
 
 /** \class EhFrameHdr
  *  \brief EhFrameHdr represents .eh_frame_hdr section.
  */
 class EhFrameHdr
 {
+public:
+  EhFrameHdr(const EhFrame& pEhFrameData,
+             const LDSection& pEhFrameSect,
+             LDSection& pEhFrameHdrSect);
 
+  ~EhFrameHdr();
+
+  /// sizeOutput - base on the fde count to size output
+  void sizeOutput();
+
+  /// emitOutput - write out eh_frame_hdr
+  template<size_t size>
+  void emitOutput(Output& pOutput, MCLinker& pLinker);
+
+private:
+  /// getFDEPC - return the address of FDE's pc
+  /// @param pFDE - FDE
+  /// @param pOffset - the output offset of FDE
+  template<size_t size>
+  typename SizeTraits<size>::Address
+  getFDEPC(const FDE* pFDE,
+           typename SizeTraits<size>::Offset pOffset,
+           Output& pOutput);
+
+  template<size_t size>
+  class BSTEntry
+  {
+  public:
+    typedef std::pair<typename SizeTraits<size>::Address,
+                      typename SizeTraits<size>::Address> EntryType;
+  };
+
+  template<size_t size>
+  struct BSTEntryCompare
+    : public std::binary_function<const typename BSTEntry<size>::EntryType&,
+                                  const typename BSTEntry<size>::EntryType&,
+                                  bool>
+  {
+    bool operator()(const typename BSTEntry<size>::EntryType& X,
+                    const typename BSTEntry<size>::EntryType& Y) const
+    { return X.first < Y.first; }
+  };
+
+private:
+  /// eh_frame data
+  const EhFrame& m_EhFrameData;
+
+  /// .eh_frame section
+  const LDSection& m_EhFrameSect;
+
+  /// .eh_frame_hdr section
+  LDSection& m_EhFrameHdrSect;
 };
+
+#include "EhFrameHdr.tcc"
 
 } // namespace of mcld
 
--- lib/LD/EhFrameHdr.cpp
+++ lib/LD/EhFrameHdr.cpp
@@ -12,4 +12,38 @@ using namespace mcld;
 
 //==========================
 // EhFrameHdr
+EhFrameHdr::EhFrameHdr(const EhFrame& pEhFrameData,
+                       const LDSection& pEhFrameSect,
+                       LDSection& pEhFrameHdrSect)
+  : m_EhFrameData(pEhFrameData),
+    m_EhFrameSect(pEhFrameSect),
+    m_EhFrameHdrSect(pEhFrameHdrSect)
+{
+}
+
+EhFrameHdr::~EhFrameHdr()
+{
+}
+
+/// @ref lsb core generic 4.1
+/// .eh_frame_hdr section format
+/// uint8_t : version
+/// uint8_t : eh_frame_ptr_enc
+/// uint8_t : fde_count_enc
+/// uint8_t : table_enc
+/// uint32_t : eh_frame_ptr
+/// uint32_t : fde_count
+/// __________________________ when fde_count > 0
+/// <uint32_t, uint32_t>+ : binary search table
+
+/// sizeOutput - base on the fde count to size output
+void EhFrameHdr::sizeOutput()
+{
+  size_t size = 12;
+  if (m_EhFrameData.canRecognizeAllEhFrame()) {
+    size_t fde_count = m_EhFrameData.getFDECount();
+    size += 8 * fde_count;
+  }
+  m_EhFrameHdrSect.setSize(size);
+}
 
