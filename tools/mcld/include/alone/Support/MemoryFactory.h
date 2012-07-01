7351055a569c33854e8a4fe411141d9f88db3151
Luba Tang <lubatang@gmail.com>
2012-07-02 01:51:40 +0800

    Add alone::MemoryFactory
    alone::MemoryFactory is a class adapter for mcld::MemoryAreaFactory.
    We try to keep dirty file descriptor operation outside of MemoryAreaFactory.

---
+++ tools/mcld/include/alone/Support/MemoryFactory.h
@@ -0,0 +1,40 @@
+//===- MemoryFactory.h ----------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef ALONE_SUPPORT_MEMORY_FACTORY_H
+#define ALONE_SUPPORT_MEMORY_FACTORY_H
+
+#include <mcld/Support/FileHandle.h>
+#include <mcld/Support/MemoryAreaFactory.h>
+
+namespace mcld {
+class MemoryArea;
+} // end namespace mcld
+
+namespace alone {
+
+class MemoryFactory : public mcld::MemoryAreaFactory
+{
+public:
+  MemoryFactory();
+
+  ~MemoryFactory() { }
+
+  using mcld::MemoryAreaFactory::produce;
+
+  mcld::MemoryArea* produce(void *pMemBuffer, size_t pSize)
+  { return mcld::MemoryAreaFactory::create(pMemBuffer, pSize); }
+
+  mcld::MemoryArea* produce(int pFD)
+  { return mcld::MemoryAreaFactory::create(pFD, mcld::FileHandle::Unknown); }
+};
+
+} // end namespace alone
+
+#endif // ALONE_SUPPORT_MEMORY_FACTORY_H
