f83922e43d9509b62abcc219c2d59db2c6074977
Luba Tang <lubatang@gmail.com>
2012-07-04 11:50:58 +0800

    Fix a bug - plasma_mips needs to specify -mtriple

--- test/Android/Plasma/MIPS/plasma_mips.ts
+++ test/Android/Plasma/MIPS/plasma_mips.ts
@@ -1,5 +1,6 @@
 ; Build the shared library.
 ; RUN: %MCLinker -filetype=dso -march=mipsel -soname=libplasma.so \
+; RUN: -mtriple="mipsel-none-linux-gnu" \
 ; RUN: -L=%p/../../../libs/MIPS/Android/android-14  -Bsymbolic \
 ; RUN: %p/../../../libs/MIPS/Android/android-14/crtbegin_so.o \
 ; RUN: %p/plasma.o \
