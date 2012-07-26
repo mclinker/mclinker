23ad08c8e5432dc9e779b37d74615e6bda06d627
JerkKao <jerkkao@gmail.com>
2012-07-26 19:55:41 +0800

    Add new regression test for alone linker.

--- test/Android/Plasma/MIPS/plasma_mips.ts
+++ test/Android/Plasma/MIPS/plasma_mips.ts
@@ -10,3 +10,13 @@
 
 ; RUN: diff -s libplasma.so %p/libplasma.golden.so
 
+; RUN: %AloneLinker --shared -soname=libplasma.so \
+; RUN: -mtriple="mipsel-none-linux-gnu" \
+; RUN: -L=%p/../../../libs/MIPS/Android/android-14  -Bsymbolic \
+; RUN: %p/../../../libs/MIPS/Android/android-14/crtbegin_so.o \
+; RUN: %p/plasma.o \
+; RUN: -o libplasma.mcld.so \
+; RUN: -lm -llog -ljnigraphics -lc  \
+; RUN: %p/../../../libs/MIPS/Android/android-14/crtend_so.o
+
+; RUN: diff -s libplasma.mcld.so %p/libplasma.golden.so
--- test/Android/Quake/ARM/quake_arm.ts
+++ test/Android/Quake/ARM/quake_arm.ts
@@ -1,12 +1,22 @@
 ; Build the shared library.
 ; RUN: %MCLinker --shared -soname=libquake.so \
 ; RUN: -march=arm -mtriple="armv7-none-linux-gnueabi" \
-; RUN: -o libquake.so \
+; RUN: -o libquake.llvm.so \
 ; RUN: -L=%p/../../../libs/ARM/Android/android-14  -Bsymbolic \
 ; RUN: %p/../../../libs/ARM/Android/android-14/crtbegin_so.o \
 ; RUN: %p/quake.o \
 ; RUN: -llog -lEGL -lGLESv1_CM -lOpenSLES -lc -lm -lstdc++ \
 ; RUN: %p/../../../libs/ARM/Android/android-14/crtend_so.o
 
-; RUN: diff -s libquake.so %p/libquake.golden.so
+; RUN: diff -s libquake.llvm.so %p/libquake.golden.so
 
+; RUN: %AloneLinker --shared -soname=libquake.so \
+; RUN: -mtriple="armv7-none-linux-gnueabi" \
+; RUN: -o libquake.mcld.so \
+; RUN: -L=%p/../../../libs/ARM/Android/android-14  -Bsymbolic \
+; RUN: %p/../../../libs/ARM/Android/android-14/crtbegin_so.o \
+; RUN: %p/quake.o \
+; RUN: -llog -lEGL -lGLESv1_CM -lOpenSLES -lc -lm -lstdc++ \
+; RUN: %p/../../../libs/ARM/Android/android-14/crtend_so.o
+
+; RUN: diff -s libquake.mcld.so %p/libquake.golden.so
