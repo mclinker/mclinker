45a6b64b4abcfe8e151801ad05203e34307fdb12
Luba Tang <lubatang@gmail.com>
2012-08-28 00:43:11 +0800

    Add SectionData::reference/const_reference and SectionData::front()/back().

--- include/mcld/LD/SectionData.h
+++ include/mcld/LD/SectionData.h
@@ -34,6 +34,10 @@ private:
 
 public:
   typedef llvm::iplist<Fragment> FragmentListType;
+
+  typedef FragmentListType::reference reference;
+  typedef FragmentListType::const_reference const_reference;
+
   typedef FragmentListType::iterator iterator;
   typedef FragmentListType::const_iterator const_iterator;
 
@@ -51,21 +55,23 @@ public:
   FragmentListType &getFragmentList() { return m_Fragments; }
   const FragmentListType &getFragmentList() const { return m_Fragments; }
 
-  iterator begin() { return m_Fragments.begin(); }
-  const_iterator begin() const { return m_Fragments.begin(); }
-
-  iterator end() { return m_Fragments.end(); }
-  const_iterator end() const { return m_Fragments.end(); }
-
-  reverse_iterator rbegin() { return m_Fragments.rbegin(); }
-  const_reverse_iterator rbegin() const { return m_Fragments.rbegin(); }
-
-  reverse_iterator rend() { return m_Fragments.rend(); }
-  const_reverse_iterator rend() const { return m_Fragments.rend(); }
-
   size_t size() const { return m_Fragments.size(); }
 
   bool empty() const { return m_Fragments.empty(); }
+
+  reference              front ()       { return m_Fragments.front();  }
+  const_reference        front () const { return m_Fragments.front();  }
+  reference              back  ()       { return m_Fragments.back();   }
+  const_reference        back  () const { return m_Fragments.back();   }
+
+  const_iterator         begin () const { return m_Fragments.begin();  }
+  iterator               begin ()       { return m_Fragments.begin();  }
+  const_iterator         end   () const { return m_Fragments.end();    }
+  iterator               end   ()       { return m_Fragments.end();    }
+  const_reverse_iterator rbegin() const { return m_Fragments.rbegin(); }
+  reverse_iterator       rbegin()       { return m_Fragments.rbegin(); }
+  const_reverse_iterator rend  () const { return m_Fragments.rend();   }
+  reverse_iterator       rend  ()       { return m_Fragments.rend();   }
 
 private:
   FragmentListType m_Fragments;
