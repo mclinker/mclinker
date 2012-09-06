556111e6b93196574dcfd614dd23342a40d25ce2
Luba Tang <lubatang@gmail.com>
2012-09-06 13:42:31 +0800

    Add AttributeOptions class.

---
+++ lib/Core/AttributeOptions.cpp
@@ -0,0 +1,22 @@
+//===- AttributeOptions.cpp -----------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include <mcld/AttributeOptions.h>
+
+using namespace mcld;
+
+//===----------------------------------------------------------------------===//
+// AttributeOptions
+//===----------------------------------------------------------------------===//
+AttributeOptions::AttributeOptions()
+{
+}
+
+AttributeOptions::~AttributeOptions()
+{
+}
