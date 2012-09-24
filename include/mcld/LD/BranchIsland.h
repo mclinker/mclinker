9d60253acd8be728808cae65051a7f38088ef460
Chinyen Chou <petechou@gmail.com>
2012-09-24 16:33:45 +0800

    Add BranchIsland interface

--- include/mcld/LD/BranchIsland.h
+++ include/mcld/LD/BranchIsland.h
@@ -6,14 +6,27 @@
 // License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
-#ifndef BRANCHISLAND_H
-#define BRANCHISLAND_H
+#ifndef MCLD_LD_BRANCH_ISLAND_H
+#define MCLD_LD_BRANCH_ISLAND_H
 #ifdef ENABLE_UNITTEST
 #include <gtest.h>
 #endif
 
+#include <llvm/Support/DataTypes.h>
+#include <llvm/ADT/StringRef.h>
+#include <mcld/ADT/HashEntry.h>
+#include <mcld/ADT/HashTable.h>
+#include <mcld/ADT/StringHash.h>
+#include <mcld/LD/SectionData.h>
+#include <mcld/LD/LDSymbol.h>
+#include <mcld/Fragment/Stub.h>
+#include <string>
+
 namespace mcld
 {
+
+class Stub;
+class Relocation;
 
 /** \class BranchIsland
  *  \brief BranchIsland is a collection of stubs
@@ -21,7 +34,113 @@ namespace mcld
  */
 class BranchIsland
 {
+public:
+  typedef SectionData::iterator iterator;
+  typedef SectionData::const_iterator const_iterator;
 
+public:
+  /*
+   *               ----------
+   *  --- Entry -> | Island | -> Exit ---
+   *               ----------
+   */
+
+  /// BranchIsland - constructor
+  /// @param pEntryFrag - the entry fragment to the island
+  /// @param pMaxSize   - the max size the island can be
+  /// @param pIndex     - the inedx in the island factory
+  BranchIsland(Fragment& pEntryFrag, size_t pMaxSize, size_t pIndex);
+
+  ~BranchIsland();
+
+  /// fragment iterators of the island
+  iterator begin();
+
+  const_iterator begin() const;
+
+  iterator end();
+
+  const_iterator end() const;
+
+  /// observers
+  uint64_t offset() const;
+
+  size_t size() const;
+
+  size_t maxSize() const;
+
+  const std::string& name() const;
+
+  size_t numOfStubs() const;
+
+  /// findStub - return true if there is a stub built from the given prototype
+  ///            for the given relocation
+  Stub* findStub(const Stub* pPrototype, const Relocation& pReloc);
+
+  /// addStub - add a stub into the island
+  bool addStub(const Stub* pPrototype, const Relocation& pReloc, Stub& pStub);
+
+private:
+  /** \class Key
+   *  \brief Key to recognize a stub in the island.
+   *
+   */
+  class Key
+  {
+  public:
+    Key(const Stub* pPrototype, const LDSymbol* pSymbol, Stub::SWord pAddend)
+    : m_pPrototype(pPrototype), m_pSymbol(pSymbol), m_Addend(pAddend)
+    { }
+
+    ~Key()
+    { }
+
+    const Stub*  prototype() const { return m_pPrototype; }
+
+    const LDSymbol* symbol() const { return m_pSymbol; }
+
+    Stub::SWord     addend() const { return m_Addend; }
+
+    struct Hash
+    {
+      size_t operator() (const Key& KEY) const
+      {
+        llvm::StringRef sym_name(KEY.symbol()->name());
+        StringHash<ELF> str_hasher;
+        return (size_t((uintptr_t)KEY.prototype())) ^
+               str_hasher(sym_name) ^
+               KEY.addend();
+      }
+    };
+
+    struct Compare
+    {
+      bool operator() (const Key& KEY1, const Key& KEY2) const
+      {
+        return (KEY1.prototype() == KEY2.prototype()) &&
+               (KEY1.symbol() == KEY2.symbol()) &&
+               (KEY1.addend() == KEY2.addend());
+      }
+    };
+
+  private:
+    const Stub* m_pPrototype;
+    const LDSymbol* m_pSymbol;
+    Stub::SWord m_Addend;
+  };
+
+  typedef HashEntry<Key, Stub*, Key::Compare> StubEntryType;
+
+  typedef HashTable<StubEntryType,
+                    Key::Hash,
+                    EntryFactory<StubEntryType> > StubMapType;
+private:
+  Fragment& m_Entry; // entry fragment of the island
+  Fragment* m_pExit; // exit fragment of the island
+  Fragment* m_pRear; // rear fragment of the island
+  size_t m_MaxSize;
+  std::string m_Name;
+  StubMapType m_StubMap;
 };
 
 } // namespace of mcld
