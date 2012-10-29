2af2bc4355e8e3ba75cb32e3a229705826e051a9
Luba Tang <lubatang@gmail.com>
2012-10-29 20:58:14 +0800

    Initialize ObjectBuilder's interface.

--- lib/Object/ObjectBuilder.cpp
+++ lib/Object/ObjectBuilder.cpp
@@ -7,12 +7,14 @@
 //
 //===----------------------------------------------------------------------===//
 #include <mcld/Object/ObjectBuilder.h>
+
 #include <mcld/Module.h>
 #include <mcld/LinkerConfig.h>
 #include <mcld/Object/SectionMap.h>
-#include <mcld/LD/SectionRules.h>
 #include <mcld/LD/LDSection.h>
 #include <mcld/LD/SectionData.h>
+#include <mcld/LD/RelocData.h>
+#include <mcld/LD/EhFrame.h>
 #include <mcld/Fragment/AlignFragment.h>
 #include <mcld/Fragment/NullFragment.h>
 
@@ -22,7 +24,7 @@ using namespace mcld;
 // ObjectBuilder
 //===----------------------------------------------------------------------===//
 ObjectBuilder::ObjectBuilder(const LinkerConfig& pConfig, Module& pTheModule)
-  : m_Config(pConfig), m_Module(pTheModule), m_pRules(NULL) {
+  : m_Config(pConfig), m_Module(pTheModule) {
 }
 
 /// CreateSection - create output section.
@@ -32,83 +34,81 @@ LDSection* ObjectBuilder::CreateSection(const std::string& pName,
                                         uint32_t pFlag,
                                         uint32_t pAlign)
 {
-  LDSection* result = NULL;
-  if (NULL == m_pRules)
-    result = LDSection::Create(pName, pKind, pType, pFlag, pAlign);
-  else {
-    result = m_pRules->getMatchedSection(pName);
-    if (NULL != result)
-      return result;
-    else {
-      const SectionMap::NamePair& new_name =
-                                   m_Config.scripts().sectionMap().find(pName);
-      if (!new_name.isNull()) {
-        // Find a rule in the link script. Add the rule in SectionRules.
-        result = LDSection::Create(new_name.to, pKind, pType, pFlag, pAlign);
-        m_pRules->append(new_name.from, *result);
-      }
-      else {
-        // Not found a rule in the link script.
-        // Sections with identical name will be merged togather, therefore, add
-        // a rule in SectionRules to merge sections with identical name.
-        result = LDSection::Create(pName, pKind, pType, pFlag, pAlign);
-        m_pRules->append(pName, *result);
-      }
-    }
-  }
-
-  SectionData* data = SectionData::Create(*result);
-  result->setSectionData(data);
-  m_Module.getSectionTable().push_back(result);
-  return result;
+  // try to get one from output LDSection
+  const SectionMap::NamePair& pair = m_Config.scripts().sectionMap().find(pName);
+  std::string output_name = (pair.isNull())?pName:pair.to;
+  LDSection* output_sect = LDSection::Create(output_name, pKind, pType, pFlag);
+  output_sect->setAlign(pAlign);
+  m_Module.getSectionTable().push_back(output_sect);
+  return output_sect;
 }
 
+/// CreateSectionData - To create a section data for given pSection.
+SectionData* ObjectBuilder::CreateSectionData(LDSection& pSection)
+{
+  assert(!pSection.hasSectionData() && "pSection already has section data.");
+
+  SectionData* sect_data = SectionData::Create(pSection);
+  pSection.setSectionData(sect_data);
+  return sect_data;
+}
+
+/// CreateRelocData - To create a relocation data for given pSection.
+RelocData* ObjectBuilder::CreateRelocData(LDSection &pSection)
+{
+  assert(!pSection.hasRelocData() && "pSection already has relocation data.");
+  RelocData* reloc_data = RelocData::Create(pSection);
+  pSection.setRelocData(reloc_data);
+  return reloc_data;
+}
+
+/// CreateEhFrame - To create a eh_frame for given pSection
+EhFrame* ObjectBuilder::CreateEhFrame(LDSection& pSection)
+{
+  return NULL;
+}
+
+/// MergeSection - merge the pInput section to the pOutput section
 bool ObjectBuilder::MergeSection(LDSection& pOutput, LDSection& pInput)
 {
-  // Step 1. Get the last offset in the output section.
-  uint64_t offset = 0;
-  if (!pOutput.getSectionData()->empty()) {
-    Fragment* last = &(pOutput.getSectionData()->back());
-    offset = last->getOffset() + last->size();
-  }
-
-  // Step 2. Insert and update alignment
-  AlignFragment* align_frag = NULL;
-  bool should_align = (pInput.align() > pOutput.align()) && (pInput.align() > 1);
-  if (!pOutput.getSectionData()->empty() && should_align) {
-    // insert AlignFragment into SectionData first if alignment constraint is
-    // not trivial and pFrag is not the first fragment of the section data.
-    align_frag = new AlignFragment(pInput.align(), // alignment
-                                   0x0, // the filled value
-                                   1u,  // the size of filled value
-                                   pInput.align() - 1, // max bytes to emit
-                                   pOutput.getSectionData());
-
-    align_frag->setOffset(offset);
-    offset += align_frag->size();
-  }
-
-  if (should_align)
-    pOutput.setAlign(pInput.align());
-
-  // Step 3. Move fragments from pInput section to pOutput section.
-  SectionData::iterator frag, fragEnd = pInput.getSectionData()->end();
-  for (frag = pInput.getSectionData()->begin(); frag != fragEnd; ++frag) {
-    frag->setParent(pOutput.getSectionData());
-    frag->setOffset(offset);
-    offset += frag->size();
-  }
-
-  pOutput.getSectionData()->getFragmentList().splice(
-                                   pOutput.getSectionData()->end(),
-                                   pInput.getSectionData()->getFragmentList());
-
-  // Step 4. Add NullFragment as the end of the pOutput section.
-  NullFragment* null = new NullFragment(pOutput.getSectionData());
-  null->setOffset(offset);
-
-  // Step 5. set up output section size.
-  pOutput.setSize(offset);
   return true;
+}
+
+/// AppendFragment - To append pFrag to the given LDSection pSection.
+/// In order to keep the alignment of pFrag, This function may insert an
+/// AlignFragment before pFrag to keep align constraint.
+uint64_t ObjectBuilder::AppendFragment(Fragment& pFrag,
+                                       LDSection& pSection,
+                                       uint32_t pAlignConstraint)
+{
+  return 0;
+}
+
+/// AppendFragment - To append pFrag to the given SectionData pSD.
+uint64_t ObjectBuilder::AppendFragment(Fragment& pFrag,
+                                       SectionData& pSD,
+                                       uint32_t pAlignConstraint)
+{
+  return 0;
+}
+
+/// AppendRelocation - To append an relocation to the given RelocData pRD.
+uint64_t
+ObjectBuilder::AppendRelocation(Relocation& pRelocation, RelocData& pRD)
+{
+  return 0;
+}
+
+/// AppendEhFrame - To append a FDE to the given EhFrame pEhFram.
+uint64_t
+ObjectBuilder::AppendEhFrame(EhFrame::FDE& pFDE, EhFrame& pRD)
+{
+  return 0;
+}
+
+/// AppendEhFrame - To append a CIE to the given EhFrame pEhFram.
+uint64_t ObjectBuilder::AppendEhFrame(EhFrame::CIE& pCIE, EhFrame& pRD)
+{
+  return 0;
 }
 
