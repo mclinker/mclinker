b583186cdf66401ef993fd55c59e9ad16d397f00
Diana Chen <diana.chen@mediatek.com>
2012-11-22 14:00:35 +0800

    Refine regression test got0.ts, link the object file directly

--- test/PLT/ARM/got0.ts
+++ test/PLT/ARM/got0.ts
@@ -1,22 +1,14 @@
 ; Build the shared library.
-; RUN: clang++ -emit-llvm -c -target arm-none-linux-gnueabi \
-; RUN: %p/gotplt.cpp -o Output/gotplt.bc
-
-; RUN: %MCLinker -filetype=obj \
-; RUN: -march=arm -mtriple arm-none-linux-gnueabi \
-; RUN: -relocation-model=pic \
-; RUN: -dB Output/gotplt.bc -o Output/gotplt.o
-
 ; RUN: %MCLinker --shared -soname=libgotplt.so \
 ; RUN: -march=arm -mtriple arm-none-linux-gnueabi \
-; RUN: Output/gotplt.o -o Output/libgotplt.so
+; RUN: %p/gotplt.o -o %t.so
 
 ; Read data from the shared library for comparison.
-; RUN: readelf -a Output/libgotplt.so | \
+; RUN: readelf -a %t.so | \
 ; RUN: grep -o "\.dynamic *DYNAMIC *[0-9a-f]*" | \
 ; RUN: awk '{print $3}' > %t
 
-; RUN: llvm-objdump -s Output/libgotplt.so | \
+; RUN: llvm-objdump -s %t.so | \
 ; RUN: grep -A 1 "section .got" | \
 ; RUN: awk 'NR==2{print $2}' | rev | dd conv=swab >> %t
 
