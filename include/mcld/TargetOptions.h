1ccbb3cf90460985889451c6b5c535b3215b68cf
Luba Tang <lubatang@gmail.com>
2012-11-28 11:51:15 +0800

    Add bitclass and endian to target options.

--- include/mcld/TargetOptions.h
+++ include/mcld/TargetOptions.h
@@ -25,6 +25,13 @@ namespace mcld {
 class TargetOptions
 {
 public:
+  enum Endian {
+    Little,
+    Big,
+    Unknown
+  };
+
+public:
   TargetOptions();
 
   TargetOptions(const std::string& pTriple);
@@ -37,8 +44,24 @@ public:
 
   void setTriple(const llvm::Triple& pTriple);
 
+  Endian endian() const { return m_Endian; }
+
+  void setEndian(Endian pEndian) { m_Endian = pEndian; }
+
+  bool isLittleEndian() const { return (Little == m_Endian); }
+  bool isBigEndian   () const { return (Big    == m_Endian); }
+
+  unsigned int bitclass() const { return m_BitClass; }
+
+  void setBitClass(unsigned int pBitClass) { m_BitClass = pBitClass; }
+
+  bool is32Bits() const { return (32 == m_BitClass); }
+  bool is64Bits() const { return (64 == m_BitClass); }
+
 private:
   llvm::Triple m_Triple;
+  Endian m_Endian;
+  unsigned int m_BitClass;
 
 };
 
