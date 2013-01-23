077c671bb1963148747a649e6bdfa97a66c7c6e9
Luba Tang <lubatang@gmail.com>
2013-01-23 11:27:25 +0800

    Add HexagonGNUInfo.

---
+++ lib/Target/Hexagon/HexagonGNUInfo.h
@@ -0,0 +1,33 @@
+//===- HexagonInfo.h -------------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef MCLD_TARGET_HEXAGON_GNU_INFO_H
+#define MCLD_TARGET_HEXAGON_GNU_INFO_H
+#include <mcld/Target/GNUInfo.h>
+
+#include <llvm/Support/ELF.h>
+
+namespace mcld {
+
+class HexagonGNUInfo : public GNUInfo
+{
+public:
+  HexagonGNUInfo(const llvm::Triple& pTriple) : GNUInfo(pTriple) { }
+
+  uint32_t machine() const { return llvm::ELF::EM_HEXAGON; }
+
+  uint64_t defaultTextSegmentAddr() const { return 0x0; }
+
+  /// flags - the value of ElfXX_Ehdr::e_flags
+  uint64_t flags() const { return 0x2; }
+};
+
+} // namespace of mcld
+
+#endif
+
