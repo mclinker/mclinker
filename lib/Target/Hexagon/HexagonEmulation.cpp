73b89d941b1462831e116c9d366293f04299fe0b
Shankar Easwaran <seaswara@qualcomm.com>
2013-01-29 11:08:01 +0800

    Add HexagonEmulation function.

---
+++ lib/Target/Hexagon/HexagonEmulation.cpp
@@ -0,0 +1,64 @@
+//===- HexagonEmulation.cpp ---------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include "Hexagon.h"
+#include <mcld/LinkerConfig.h>
+#include <mcld/Target/ELFEmulation.h>
+#include <mcld/Support/TargetRegistry.h>
+
+namespace mcld {
+
+static bool MCLDEmulateHexagonELF(LinkerConfig& pConfig)
+{
+  if (!MCLDEmulateELF(pConfig))
+    return false;
+
+  // set up bitclass and endian
+  pConfig.targets().setEndian(TargetOptions::Little);
+  pConfig.targets().setBitClass(32);
+
+  // set up target-dependent constraints of attributes
+  pConfig.attribute().constraint().enableWholeArchive();
+  pConfig.attribute().constraint().enableAsNeeded();
+  pConfig.attribute().constraint().setSharedSystem();
+
+  // set up the predefined attributes
+  pConfig.attribute().predefined().unsetWholeArchive();
+  pConfig.attribute().predefined().unsetAsNeeded();
+  pConfig.attribute().predefined().setDynamic();
+  return true;
+}
+
+//===----------------------------------------------------------------------===//
+// emulateHexagonLD - the help function to emulate Hexagon ld
+//===----------------------------------------------------------------------===//
+bool emulateHexagonLD(const std::string& pTriple, LinkerConfig& pConfig)
+{
+  llvm::Triple theTriple(pTriple);
+  if (theTriple.isOSDarwin()) {
+    assert(0 && "MachO linker has not supported yet");
+    return false;
+  }
+  if (theTriple.isOSWindows()) {
+    assert(0 && "COFF linker has not supported yet");
+    return false;
+  }
+
+  return MCLDEmulateHexagonELF(pConfig);
+}
+
+} // namespace of mcld
+
+//===----------------------------------------------------------------------===//
+// HexagonEmulation
+//===----------------------------------------------------------------------===//
+extern "C" void MCLDInitializeHexagonEmulation() {
+  // Register the emulation
+  mcld::TargetRegistry::RegisterEmulation(mcld::TheHexagonTarget, mcld::emulateHexagonLD);
+}
+
