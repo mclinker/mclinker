ddc8c4e59394a04981cb34278a74035d74dad3e9
Luba Tang <lubatang@gmail.com>
2013-01-30 13:32:51 +0800

    Add Hexagon backend.

---
+++ lib/Target/Hexagon/HexagonLDBackend.cpp
@@ -0,0 +1,272 @@
+//===- HexagonLDBackend.cpp -----------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include "Hexagon.h"
+#include "HexagonELFDynamic.h"
+#include "HexagonLDBackend.h"
+#include "HexagonRelocator.h"
+#include "HexagonGNUInfo.h"
+
+#include <llvm/ADT/Triple.h>
+#include <llvm/Support/Casting.h>
+
+#include <mcld/LinkerConfig.h>
+#include <mcld/IRBuilder.h>
+#include <mcld/Fragment/FillFragment.h>
+#include <mcld/Fragment/RegionFragment.h>
+#include <mcld/Fragment/FragmentLinker.h>
+#include <mcld/Support/MemoryRegion.h>
+#include <mcld/Support/MsgHandling.h>
+#include <mcld/Support/TargetRegistry.h>
+#include <mcld/Object/ObjectBuilder.h>
+
+#include <cstring>
+
+using namespace mcld;
+
+//===----------------------------------------------------------------------===//
+// HexagonLDBackend
+//===----------------------------------------------------------------------===//
+HexagonLDBackend::HexagonLDBackend(const LinkerConfig& pConfig, 
+                                   HexagonGNUInfo* pInfo)
+  : GNULDBackend(pConfig, pInfo),
+    m_pRelocator(NULL),
+    m_pGOT(NULL),
+    m_pPLT(NULL),
+    m_pRelDyn(NULL),
+    m_pRelPLT(NULL),
+    m_pDynamic(NULL),
+    m_pGOTSymbol(NULL) {
+}
+
+HexagonLDBackend::~HexagonLDBackend()
+{
+  delete m_pRelocator;
+  delete m_pGOT;
+  delete m_pPLT;
+  delete m_pRelDyn;
+  delete m_pRelPLT;
+  delete m_pDynamic;
+}
+
+bool HexagonLDBackend::initRelocator(const FragmentLinker& pLinker)
+{
+  if (NULL == m_pRelocator) {
+    m_pRelocator = new HexagonRelocator(*this);
+    m_pRelocator->setFragmentLinker(pLinker);
+  }
+  return true;
+}
+
+Relocator* HexagonLDBackend::getRelocator()
+{
+  assert(NULL != m_pRelocator);
+  return m_pRelocator;
+}
+
+void HexagonLDBackend::doPreLayout(FragmentLinker& pLinker)
+{
+}
+
+void HexagonLDBackend::doPostLayout(Module& pModule,
+                                   FragmentLinker& pLinker)
+{
+}
+
+/// dynamic - the dynamic section of the target machine.
+/// Use co-variant return type to return its own dynamic section.
+HexagonELFDynamic& HexagonLDBackend::dynamic()
+{
+  if (NULL == m_pDynamic)
+    m_pDynamic = new HexagonELFDynamic(*this, config());
+
+  return *m_pDynamic;
+}
+
+/// dynamic - the dynamic section of the target machine.
+/// Use co-variant return type to return its own dynamic section.
+const HexagonELFDynamic& HexagonLDBackend::dynamic() const
+{
+  assert( NULL != m_pDynamic);
+  return *m_pDynamic;
+}
+
+void HexagonLDBackend::scanRelocation(Relocation& pReloc,
+                                     FragmentLinker& pLinker,
+                                     Module& pModule,
+                                     LDSection& pSection)
+{
+}
+
+uint64_t HexagonLDBackend::emitSectionData(const LDSection& pSection,
+                                          MemoryRegion& pRegion) const
+{
+}
+
+HexagonGOT& HexagonLDBackend::getGOT()
+{
+  assert(NULL != m_pGOT);
+  return *m_pGOT;
+}
+
+const HexagonGOT& HexagonLDBackend::getGOT() const
+{
+  assert(NULL != m_pGOT);
+  return *m_pGOT;
+}
+
+HexagonPLT& HexagonLDBackend::getPLT()
+{
+  assert(NULL != m_pPLT && "PLT section not exist");
+  return *m_pPLT;
+}
+
+const HexagonPLT& HexagonLDBackend::getPLT() const
+{
+  assert(NULL != m_pPLT && "PLT section not exist");
+  return *m_pPLT;
+}
+
+OutputRelocSection& HexagonLDBackend::getRelDyn()
+{
+  assert(NULL != m_pRelDyn && ".rel.dyn section not exist");
+  return *m_pRelDyn;
+}
+
+const OutputRelocSection& HexagonLDBackend::getRelDyn() const
+{
+  assert(NULL != m_pRelDyn && ".rel.dyn section not exist");
+  return *m_pRelDyn;
+}
+
+OutputRelocSection& HexagonLDBackend::getRelPLT()
+{
+  assert(NULL != m_pRelPLT && ".rel.plt section not exist");
+  return *m_pRelPLT;
+}
+
+const OutputRelocSection& HexagonLDBackend::getRelPLT() const
+{
+  assert(NULL != m_pRelPLT && ".rel.plt section not exist");
+  return *m_pRelPLT;
+}
+
+unsigned int
+HexagonLDBackend::getTargetSectionOrder(const LDSection& pSectHdr) const
+{
+  const ELFFileFormat* file_format = getOutputFormat();
+
+  if (&pSectHdr == &file_format->getGOT()) {
+    if (config().options().hasNow())
+      return SHO_RELRO;
+    return SHO_RELRO_LAST;
+  }
+
+  if (&pSectHdr == &file_format->getPLT())
+    return SHO_PLT;
+
+  return SHO_UNDEFINED;
+}
+
+void HexagonLDBackend::initTargetSections(Module& pModule, 
+                                          ObjectBuilder& pBuilder)
+{
+  if (LinkerConfig::Object != config().codeGenType()) {
+    ELFFileFormat* file_format = getOutputFormat();
+    // initialize .got
+    LDSection& got = file_format->getGOT();
+    m_pGOT = new HexagonGOT(got);
+
+    // initialize .plt
+    LDSection& plt = file_format->getPLT();
+    m_pPLT = new HexagonPLT(plt,
+                        *m_pGOT,
+                        config());
+
+    // initialize .rel.plt
+    LDSection& relplt = file_format->getRelPlt();
+    relplt.setLink(&plt);
+    m_pRelPLT = new OutputRelocSection(pModule, relplt);
+
+    // initialize .rel.dyn
+    LDSection& reldyn = file_format->getRelDyn();
+    m_pRelDyn = new OutputRelocSection(pModule, reldyn);
+
+  }
+}
+
+void HexagonLDBackend::initTargetSymbols(FragmentLinker& pLinker)
+{
+  if (LinkerConfig::Object != config().codeGenType()) {
+    // Define the symbol _GLOBAL_OFFSET_TABLE_ if there is a symbol with the
+    // same name in input
+    m_pGOTSymbol =
+      pLinker.defineSymbol<FragmentLinker::AsReferred,
+                           FragmentLinker::Resolve>("_GLOBAL_OFFSET_TABLE_",
+                                                    ResolveInfo::Object,
+                                                    ResolveInfo::Define,
+                                                    ResolveInfo::Local,
+                                                    0x0,  // size
+                                                    0x0,  // value
+                                                    FragmentRef::Null(),
+                                                    ResolveInfo::Hidden);
+  }
+}
+
+/// finalizeSymbol - finalize the symbol value
+bool HexagonLDBackend::finalizeTargetSymbols(FragmentLinker& pLinker)
+{
+  return true;
+}
+
+/// doCreateProgramHdrs - backend can implement this function to create the
+/// target-dependent segments
+void HexagonLDBackend::doCreateProgramHdrs(Module& pModule,
+                                          const FragmentLinker& pLinker)
+{
+  // TODO
+}
+
+namespace mcld {
+
+//===----------------------------------------------------------------------===//
+/// createHexagonLDBackend - the help funtion to create corresponding 
+/// HexagonLDBackend
+TargetLDBackend* createHexagonLDBackend(const llvm::Target& pTarget,
+                                    const LinkerConfig& pConfig)
+{
+  if (pConfig.targets().triple().isOSDarwin()) {
+    assert(0 && "MachO linker is not supported yet");
+    /**
+    return new HexagonMachOLDBackend(createHexagonMachOArchiveReader,
+                               createHexagonMachOObjectReader,
+                               createHexagonMachOObjectWriter);
+    **/
+  }
+  if (pConfig.targets().triple().isOSWindows()) {
+    assert(0 && "COFF linker is not supported yet");
+    /**
+    return new HexagonCOFFLDBackend(createHexagonCOFFArchiveReader,
+                               createHexagonCOFFObjectReader,
+                               createHexagonCOFFObjectWriter);
+    **/
+  }
+  return new HexagonLDBackend(pConfig, 
+                              new HexagonGNUInfo(pConfig.targets().triple()));
+}
+
+} // namespace of mcld
+
+//===----------------------------------------------------------------------===//
+// Force static initialization.
+//===----------------------------------------------------------------------===//
+extern "C" void MCLDInitializeHexagonLDBackend() {
+  // Register the linker backend
+  mcld::TargetRegistry::RegisterTargetLDBackend(TheHexagonTarget, 
+                                                createHexagonLDBackend);
+}
---
+++ lib/Target/Hexagon/HexagonLDBackend.h
@@ -0,0 +1,141 @@
+//===- HexagonLDBackend.h -------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef HEXAGON_LDBACKEND_H
+#define HEXAGON_LDBACKEND_H
+
+#include "HexagonELFDynamic.h"
+#include "HexagonGOT.h"
+#include "HexagonPLT.h"
+#include <mcld/LD/LDSection.h>
+#include <mcld/Target/GNULDBackend.h>
+#include <mcld/Target/OutputRelocSection.h>
+
+namespace mcld {
+
+class LinkerConfig;
+class HexagonGNUInfo;
+
+//===----------------------------------------------------------------------===//
+/// HexagonLDBackend - linker backend of Hexagon target of GNU ELF format
+///
+class HexagonLDBackend : public GNULDBackend
+{
+public:
+  HexagonLDBackend(const LinkerConfig& pConfig, HexagonGNUInfo* pInfo);
+
+  ~HexagonLDBackend();
+
+  uint32_t machine() const;
+
+  HexagonGOT& getGOT();
+
+  const HexagonGOT& getGOT() const;
+
+  HexagonPLT& getPLT();
+
+  const HexagonPLT& getPLT() const;
+
+  /// preLayout - Backend can do any needed modification before layout
+  void doPreLayout(FragmentLinker& pLinker);
+
+  /// postLayout -Backend can do any needed modification after layout
+  void doPostLayout(Module& pModule, FragmentLinker& pLinker);
+
+  /// dynamic - the dynamic section of the target machine.
+  /// Use co-variant return type to return its own dynamic section.
+  HexagonELFDynamic& dynamic();
+
+  /// dynamic - the dynamic section of the target machine.
+  /// Use co-variant return type to return its own dynamic section.
+  const HexagonELFDynamic& dynamic() const;
+
+  /// emitSectionData - write out the section data into the memory region.
+  /// When writers get a LDSection whose kind is LDFileFormat::Target, writers
+  /// call back target backend to emit the data.
+  ///
+  /// Backends handle the target-special tables (plt, gp,...) by themselves.
+  /// Backend can put the data of the tables in MCSectionData directly
+  ///  - LDSection.getSectionData can get the section data.
+  /// Or, backend can put the data into special data structure
+  ///  - backend can maintain its own map<LDSection, table> to get the table
+  /// from given LDSection.
+  ///
+  /// @param pSection - the given LDSection
+  /// @param pLayout - for comouting the size of fragment
+  /// @param pRegion - the region to write out data
+  /// @return the size of the table in the file.
+  uint64_t emitSectionData(const LDSection& pSection,
+                           MemoryRegion& pRegion) const;
+
+  /// initRelocator - create and initialize Relocator.
+  bool initRelocator(const FragmentLinker& pLinker);
+
+  /// getRelocator - return relocator.
+  Relocator* getRelocator();
+
+  void initTargetSections(Module& pModule, ObjectBuilder& pBuilder);
+
+  void initTargetSymbols(FragmentLinker& pLinker);
+
+  /// scanRelocation - determine the empty entries are needed or not and create
+  /// the empty entries if needed.
+  /// For Hexagon, following entries are check to create:
+  /// - GOT entry (for .got and .got.plt sections)
+  /// - PLT entry (for .plt section)
+  /// - dynamin relocation entries (for .rel.plt and .rel.dyn sections)
+  void scanRelocation(Relocation& pReloc,
+                      FragmentLinker& pLinker,
+                      Module& pModule,
+                      LDSection& pSection);
+
+  OutputRelocSection& getRelDyn();
+
+  const OutputRelocSection& getRelDyn() const;
+
+  OutputRelocSection& getRelPLT();
+
+  const OutputRelocSection& getRelPLT() const;
+
+  /// getTargetSectionOrder - compute the layout order of Hexagon target section
+  unsigned int getTargetSectionOrder(const LDSection& pSectHdr) const;
+
+  /// finalizeTargetSymbols - finalize the symbol value
+  bool finalizeTargetSymbols(FragmentLinker& pLinker);
+
+private:
+
+  /// getRelEntrySize - the size in BYTE of rel type relocation
+  size_t getRelEntrySize()
+  { return 8; }
+
+  /// getRelEntrySize - the size in BYTE of rela type relocation
+  size_t getRelaEntrySize()
+  { return 12; }
+
+  /// doCreateProgramHdrs - backend can implement this function to create the
+  /// target-dependent segments
+  virtual void doCreateProgramHdrs(Module& pModule,
+                                   const FragmentLinker& pLinker);
+
+private:
+  Relocator* m_pRelocator;
+  HexagonGOT* m_pGOT;
+  HexagonPLT* m_pPLT;
+  /// m_RelDyn - dynamic relocation table of .rel.dyn
+  OutputRelocSection* m_pRelDyn;
+  /// m_RelPLT - dynamic relocation table of .rel.plt
+  OutputRelocSection* m_pRelPLT;
+
+  HexagonELFDynamic* m_pDynamic;
+  LDSymbol* m_pGOTSymbol;
+};
+} // namespace of mcld
+
+#endif
+
