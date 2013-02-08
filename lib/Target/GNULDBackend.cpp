9f28d046769a2ccc31a25e0b4f7a8d1904d3a2b4
Chinyen Chou <petechou@gmail.com>
2013-02-08 11:52:27 +0800

    Check relro when padding PT_LOAD.

--- lib/Target/GNULDBackend.cpp
+++ lib/Target/GNULDBackend.cpp
@@ -2110,15 +2110,16 @@ void GNULDBackend::setOutputSectionAddress(Module& pModule,
       }
 
       // padding
-      if (((*seg).front()->offset() &  ((*seg).align() - 1)) != 0) {
-        int64_t segAlign = (*seg).align();
-        start_addr = (start_addr + segAlign - 1) & ~(segAlign - 1);
-        int64_t aligned_offset = ((*seg).front()->offset() + segAlign - 1)
-                                            & ~(segAlign - 1);
+      if (((*seg).front()->offset() & ((*seg).align() - 1)) != 0) {
+        uint64_t offset = (*seg).front()->offset();
+        alignAddress(offset, (*seg).align());
+        alignAddress(start_addr, (*seg).align());
         setOutputSectionOffset(pModule,
                                pModule.begin() + (*seg).front()->index(),
                                pModule.end(),
-                               aligned_offset);
+                               offset);
+        if (config().options().hasRelro())
+          setupRelro(pModule);
       }
     }
 
