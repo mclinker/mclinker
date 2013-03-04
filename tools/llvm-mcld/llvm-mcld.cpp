6ee31d92d37b246493f38fd16ae519bb170526a6
Andrew Hsieh <andrewhsieh@google.com>
2013-03-04 15:22:22 +0800

    Ignore -EL and -EB from MIPS GCC/collect2

--- tools/llvm-mcld/llvm-mcld.cpp
+++ tools/llvm-mcld/llvm-mcld.cpp
@@ -792,6 +792,19 @@ ArgTextSegAddr("Ttext",
                cl::init(-1U));
 
 //===----------------------------------------------------------------------===//
+// Ignored GCC Compatibility Options
+//===----------------------------------------------------------------------===//
+static cl::opt<bool>
+ArgEL("EL",
+      cl::desc("GCC/collect2 compatibility: MIPS little endian.  Ignored"),
+      cl::init(false));
+
+static cl::opt<bool>
+ArgEB("EB",
+      cl::desc("GCC/collect2 compatibility: MIPS big endian.  Ignored"),
+      cl::init(false));
+
+//===----------------------------------------------------------------------===//
 // non-member functions
 //===----------------------------------------------------------------------===//
 /// GetOutputStream - get the output stream.
