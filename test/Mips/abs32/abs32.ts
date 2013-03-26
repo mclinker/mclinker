ef0897984b212ddc155018832eb3367b2fa85a5f
Luba Tang <lubatang@gmail.com>
2013-03-26 19:38:38 +0800

    Add Mips abs32 testcase.

---
+++ test/Mips/abs32/abs32.ts
@@ -0,0 +1,11 @@
+; RUN: %MCLinker -mtriple=mipsel-none-linux-gnueabi \
+; RUN: --eh-frame-hdr -EL -m elf32ltsmip -shared -Bsymbolic -z noexecstack -z relro -z now \
+; RUN: --no-undefined -z noexecstack -z relro -z now \
+; RUN: -o libhello-jni.so \
+; RUN: -soname libhello-jni.so \
+; RUN: --sysroot=%p/../../libs/MIPS/Android/android-14 \
+; RUN: %p/../../libs/MIPS/Android/android-14/crtbegin_so.o \
+; RUN: -L%p/../../libs/MIPS/Android/android-14 \
+; RUN: %p/hello-jni.o \
+; RUN: -lgcc -lstdc++ -lm -lc -lgcc -lc -ldl -lgcc \
+; RUN: %p/../../libs/MIPS/Android/android-14/crtend_so.o
