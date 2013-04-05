2208bae9b611acda4ea73df416f874a77c898bae
Luba Tang <lubatang@gmail.com>
2013-04-06 04:01:23 +0800

    Set up TargetCPU and TargetFeatureString of TargetOptions.

--- tools/llvm-mcld/llvm-mcld.cpp
+++ tools/llvm-mcld/llvm-mcld.cpp
@@ -1444,6 +1444,9 @@ int main(int argc, char* argv[])
   assert(target_machine.get() && "Could not allocate target machine!");
   mcld::MCLDTargetMachine &TheTargetMachine = *target_machine.get();
 
+  LDConfig.targets().setTargetCPU(MCPU);
+  LDConfig.targets().setTargetFeatureString(FeaturesStr);
+
   TheTargetMachine.getTM().setMCUseLoc(false);
   TheTargetMachine.getTM().setMCUseCFI(false);
 
