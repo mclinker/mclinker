edf7cf7c41c72bcc37bcc077c7cb42926713b574
Chinyen Chou <petechou@gmail.com>
2013-04-15 14:21:37 +0800

    Add ScriptCommand class.
    The idea to use a command queue to record linker script commands
    is proposed by Shankar Easwaran.

---
+++ include/mcld/LD/LinkerScript/ScriptCommand.h
@@ -0,0 +1,52 @@
+//===- ScriptCommand.h ----------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef MCLD_SCRIPT_COMMAND_INTERFACE_H
+#define MCLD_SCRIPT_COMMAND_INTERFACE_H
+#ifdef ENABLE_UNITTEST
+#include <gtest.h>
+#endif
+
+namespace mcld
+{
+
+/** \class ScriptCommand
+ *  \brief This class defines the interfaces to a script command.
+ */
+
+class ScriptCommand
+{
+public:
+  enum Kind {
+    OutputFormat,
+    Group,
+  };
+
+protected:
+  ScriptCommand(Kind pKind)
+    : m_Kind(pKind)
+  {}
+
+public:
+  virtual ~ScriptCommand()
+  {}
+
+  virtual void dump() const = 0;
+
+  virtual void activate() = 0;
+
+  Kind getKind() const { return m_Kind; }
+
+private:
+  Kind m_Kind;
+};
+
+} // namespace of mcld
+
+#endif
+
