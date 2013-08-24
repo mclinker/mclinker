5334b731c2a45a3b1fa5a5f73ed844885fa0891f
Luba Tang <lubatang@gmail.com>
2013-08-25 01:08:27 +0800

    Add TripleOptions.
    TripleOptions collects all options used to set up triple.

---
+++ tools/ld.lite/include/lite/TripleOptions.h
@@ -0,0 +1,36 @@
+//===- TripleOptions.h ----------------------------------------------------===//
+//
+//                     The MCLinker Project
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef MCLD_LDLITE_TRIPLE_OPTIONS_H
+#define MCLD_LDLITE_TRIPLE_OPTIONS_H
+#include <llvm/Support/CommandLine.h>
+#include <string>
+
+namespace mcld {
+
+class LinkerConfig;
+
+class TripleOptions
+{
+public:
+  TripleOptions();
+
+  bool parse(int pArgc, char* pArgv[], LinkerConfig& pConfig);  
+
+private:
+  llvm::cl::opt<std::string>&  m_TargetTriple;
+  llvm::cl::opt<std::string>&  m_MArch;
+  llvm::cl::opt<std::string>&  m_MCPU;
+  llvm::cl::list<std::string>& m_MAttrs;
+  llvm::cl::opt<std::string>&  m_Emulation;
+};
+
+} // namespace of mcld
+
+#endif
+
