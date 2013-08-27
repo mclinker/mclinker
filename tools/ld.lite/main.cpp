b93577037abe2ce1242b9d56482581fe1805e393
Luba Tang <lubatang@gmail.com>
2013-08-27 15:56:33 +0800

    [ld.lite] Remove GeneralOptions.h.
    And also add options in the build list.

--- tools/ld.lite/main.cpp
+++ tools/ld.lite/main.cpp
@@ -6,15 +6,19 @@
 // License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+#include <lite/TripleOptions.h>
+#include <lite/OutputFormatOptions.h>
+
 #include <mcld/Module.h>
 #include <mcld/Environment.h>
 #include <mcld/LinkerConfig.h>
 #include <mcld/LinkerScript.h>
+#include <mcld/Linker.h>
+#include <mcld/IRBuilder.h>
 #include <mcld/Support/Path.h>
 #include <mcld/Support/CommandLine.h>
 #include <mcld/Support/TargetRegistry.h>
 #include <mcld/Support/MsgHandling.h>
-#include <mcld/Support/SystemUtils.h>
 #include <mcld/Support/raw_ostream.h>
 #include <llvm/ADT/StringRef.h>
 #include <llvm/ADT/StringSwitch.h>
@@ -36,160 +40,25 @@
 //      language, such as --defsym.
 //   3. General options. (the rest of options)
 //===----------------------------------------------------------------------===//
-#include "PositionalOptions.h"
-#include "ScriptOptions.h"
-#include "GeneralOptions.h"
 
-//===----------------------------------------------------------------------===//
-// Non-member functions
-//===----------------------------------------------------------------------===//
-static bool ShouldColorize()
+/// configure linker
+static inline bool ConfigLinker(int pArgc,
+                                char* pArgv[],
+                                const char* pName,
+                                mcld::LinkerScript& pScript,
+                                mcld::LinkerConfig& pConfig,
+                                std::string& pError)
 {
-   const char* term = getenv("TERM");
-   return term && (0 != strcmp(term, "dumb"));
-}
-
-static inline void
-SetUpEnvironment(mcld::LinkerConfig& pConfig)
-{
-  // set up colorize
-  switch (ArgColor) {
-    case color::Never:
-      pConfig.options().setColor(false);
-    break;
-    case color::Always:
-      pConfig.options().setColor(true);
-    break;
-    case color::Auto:
-      bool color_option = ShouldColorize() &&
-                 llvm::sys::Process::FileDescriptorIsDisplayed(STDOUT_FILENO);
-      pConfig.options().setColor(color_option);
-    break;
-  }
-
-  mcld::outs().setColor(pConfig.options().color());
-  mcld::errs().setColor(pConfig.options().color());
-}
-
-/// ParseProgName - Parse program name
-/// This function simplifies cross-compiling by reading triple from the program
-/// name. For example, if the program name is `arm-linux-eabi-ld.mcld', we can
-/// get the triple is arm-linux-eabi by the program name.
-static std::string ParseProgName(const char *progname)
-{
-  static const char *suffixes[] = {
-    "ld",
-    "ld.mcld",
-    "ld.lite",
-  };
-
-  std::string ProgName(mcld::sys::fs::Path(progname).stem().native());
-
-  for (size_t i = 0; i < sizeof(suffixes) / sizeof(suffixes[0]); ++i) {
-    if (ProgName == suffixes[i])
-      return std::string();
-  }
-
-  llvm::StringRef ProgNameRef(ProgName);
-  llvm::StringRef Prefix;
-
-  for (size_t i = 0; i < sizeof(suffixes) / sizeof(suffixes[0]); ++i) {
-    if (!ProgNameRef.endswith(suffixes[i]))
-      continue;
-
-    llvm::StringRef::size_type LastComponent = ProgNameRef.rfind('-',
-      ProgNameRef.size() - strlen(suffixes[i]));
-    if (LastComponent == llvm::StringRef::npos)
-      continue;
-    llvm::StringRef Prefix = ProgNameRef.slice(0, LastComponent);
-    std::string IgnoredError;
-    if (!mcld::TargetRegistry::lookupTarget(Prefix, IgnoredError))
-      continue;
-    return Prefix.str();
-  }
-  return std::string();
-}
-
-static inline void
-ParseEmulation(llvm::Triple& pTriple, const std::string& pEmulation)
-{
-  llvm::Triple triple = llvm::StringSwitch<llvm::Triple>(pEmulation)
-    .Case("armelf_linux_eabi", llvm::Triple("arm", "", "linux", "gnueabi"))
-    .Case("elf_i386",          llvm::Triple("i386", "", "", "gnu"))
-    .Case("elf_x86_64",        llvm::Triple("x86_64", "", "", "gnu"))
-    .Case("elf32_x86_64",      llvm::Triple("x86_64", "", "", "gnux32"))
-    .Case("elf_i386_fbsd",     llvm::Triple("i386", "", "freebsd", "gnu"))
-    .Case("elf_x86_64_fbsd",   llvm::Triple("x86_64", "", "freebsd", "gnu"))
-    .Case("elf32ltsmip",       llvm::Triple("mipsel", "", "", "gnu"))
-    .Default(llvm::Triple());
-
-  if (triple.getArch()        == llvm::Triple::UnknownArch &&
-      triple.getOS()          == llvm::Triple::UnknownOS &&
-      triple.getEnvironment() == llvm::Triple::UnknownEnvironment)
-    mcld::error(mcld::diag::err_invalid_emulation) << pEmulation << "\n";
-
-  if (triple.getArch()        != llvm::Triple::UnknownArch)
-    pTriple.setArch(triple.getArch());
-
-  if (triple.getOS()          != llvm::Triple::UnknownOS)
-    pTriple.setOS(triple.getOS());
-
-  if (triple.getEnvironment() != llvm::Triple::UnknownEnvironment)
-    pTriple.setEnvironment(triple.getEnvironment());
-
-}
-
-static inline void
-ParseTriple(int pArgc, char* pArgv[], mcld::LinkerConfig& pConfig)
-{
-  llvm::Triple triple;
-  if (!TargetTriple.empty()) {
-    // 1. Use the triple from command.
-    triple.setTriple(TargetTriple);
-  }
-  else {
-    std::string prog_triple = ParseProgName(pArgv[0]);
-    if (!prog_triple.empty()) {
-      // 2. Use the triple from the program name prefix.
-      triple.setTriple(prog_triple);
-    }
-    else {
-      // 3. Use the default target triple.
-      triple.setTriple(mcld::sys::getDefaultTargetTriple());
-    }
-  }
-
-  // If a specific emulation was requested, apply it now.
-  if (!ArgEmulation.empty())
-    ParseEmulation(triple, ArgEmulation);
-
-  pConfig.targets().setTriple(triple);
-  pConfig.targets().setTargetCPU(MCPU);
-
-  // Package up features to be passed to target/subtarget
-  std::string feature_str;
-  if (MAttrs.size()) {
-    llvm::SubtargetFeatures features;
-    for (unsigned i = 0; i != MAttrs.size(); ++i)
-      features.AddFeature(MAttrs[i]);
-    feature_str = features.getString();
-  }
-  pConfig.targets().setTargetFeatureString(feature_str);
-}
-
-static bool ParseCommandLineOptions(int pArgc,
-                                    char* pArgv[],
-                                    const char* pName,
-                                    mcld::LinkerScript& pScript,
-                                    mcld::LinkerConfig& pConfig)
-{
+  mcld::TripleOptions triple_options;
+  mcld::OutputFormatOptions output_format_options;
+  
   llvm::cl::ParseCommandLineOptions(pArgc, pArgv, pName);
 
-  SetUpEnvironment(pConfig);
+  if (!triple_options.parse(pArgc, pArgv, pConfig))
+    return false;
 
-  /// Set up general options
-  ParseTriple(pArgc, pArgv, pConfig);
-
+  if (!output_format_options.parse(pArgc, pArgv, pConfig))
+    return false;
   return true;
 }
 
@@ -199,11 +68,16 @@ int main(int argc, char* argv[])
 
   mcld::LinkerScript script;
   mcld::LinkerConfig config;
-
-  if (!ParseCommandLineOptions(argc, argv, "MCLinker\n", script, config)) {
+  std::string error;
+  if (!ConfigLinker(argc, argv, "MCLinker\n", script, config, error)) {
     mcld::errs() << argv[0]
-                 << ": failed to process linker options from command line!\n";
+                 << ": failed to process linker options from command line!\n"
+                 << "(Detail: " << error << ")\n";
     return 1;
   }
+
+  mcld::Module module(script);
+  mcld::IRBuilder builder(module, config);
+  mcld::Linker linker;
   mcld::Finalize();
 }
