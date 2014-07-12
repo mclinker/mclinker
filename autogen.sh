d6808c3d09196aaa1d393099fd5a37acca6385e9
Zonr Chang <zonr.net@gmail.com>
2014-07-12 11:17:38 +0800

    Exit immediately when one of the commands in autogen.sh fails.
    Thank Daniel Wilkerson for reporting the bug.

--- autogen.sh
+++ autogen.sh
@@ -10,11 +10,11 @@ my_uname=`uname`
 if [ ${my_uname} = "Darwin" ]; then
   LIBTOOLIZE=glibtoolize
 fi
-root=`dirname $0`
-cd $root
 
-aclocal -I ./m4
-autoheader
-${LIBTOOLIZE} --force
-automake --add-missing
+root=`dirname $0`
+cd $root && \
+aclocal -I ./m4 && \
+autoheader && \
+${LIBTOOLIZE} --force && \
+automake --add-missing && \
 autoconf
