a9c962cce597f0922c851dafcada7e418fba95c8
Pete Chou <petechou@gmail.com>
2014-07-28 11:55:00 +0800

    Remove Quake tests.

--- test/Android/Quake2/X86/quake2_x86.ts
+++
@@ -1,9 +0,0 @@
-; Build the shared library.
-; RUN: %MCLinker -filetype=dso -march=x86 -soname=libquake2.so \
-; RUN: -mtriple="x86-none-linux-gnueabi" \
-; RUN: -L=%p/../../../libs/X86/Android/android-14  -Bsymbolic \
-; RUN: %p/quake2.o \
-; RUN: -o libquake2.so \
-; RUN: -llog -lEGL -lGLESv1_CM -lOpenSLES -lc -lm
-
-; RUN: diff -s libquake2.so %p/libquake2.golden.so
