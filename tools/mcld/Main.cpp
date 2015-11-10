621f98a07c1bb0d73a44838ccf818cf0f00ec6bd
Diana Chen <mysekki@gmail.com>
2015-11-10 17:42:18 +0800

    Refine the error message

--- tools/mcld/Main.cpp
+++ tools/mcld/Main.cpp
@@ -600,11 +600,8 @@ bool Driver::TranslateArguments(llvm::opt::InputArgList& args) {
 
   // -L searchdir
   for (llvm::opt::Arg* arg : args.filtered(kOpt_LibraryPath)) {
-    if (!script_.directories().insert(arg->getValue())) {
-      // FIXME: need a warning function
-      mcld::errs() << "WARNING: can not open search directory `-L"
-                   << arg->getValue() << "'.\n";
-    }
+    if (!script_.directories().insert(arg->getValue()))
+      mcld::warning(mcld::diag::warn_cannot_open_search_dir) << arg->getValue();
   }
 
   // -nostdlib
